5b5fb55f49c6035fed4ff6142e1b3527ff568885
ChoHJin <cjc9739@naver.com>
2022-07-30 23:42:24 +0900

    0730

---
+++ boj1003.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int dp[41] = {0, 1,};
+
+int main() {
+    int t, n;
+    
+    cin >> t;
+
+    for(int i = 3; i < 41; i++) {
+        dp[i] = dp[i - 2] + dp[i - 1];
+    }
+
+    for(int i = 0; i < t; i++) {
+        cin >> n;
+        
+        if(n == 0) {
+            cout << "1 0" << endl;
+        }
+        else if (n == 1) {
+            cout << "0 1" << endl;
+        }
+        else {
+            cout << dp[n - 1] << " " << dp[n] << endl;
+        }
+    }
+}
