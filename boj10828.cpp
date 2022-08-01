e60281a4149d1d9ba3809e47c2b7ba7b0967bb75
ChoHJin <cjc9739@naver.com>
2022-08-01 23:55:01 +0900

    0801

---
+++ boj10828.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+using namespace std;
+
+int stack;
+int arr[10001];
+
+void push(int x) {
+    arr[stack] = x;
+    stack++;
+}
+
+int pop() {
+    if(stack == 0) return -1;
+    
+    int t = arr[stack - 1];
+    stack--;
+    
+    return t;
+}
+
+int top() {
+    if(stack == 0) {
+        return -1;
+    }
+
+    return arr[stack - 1];
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+        string s;
+        cin >> s;
+
+        if(s == "push") {
+            int n;
+            cin >> n;
+            push(n);
+        }
+
+        if(s == "pop") {
+            cout << pop() << "\n";
+        }
+
+        if(s == "size") {
+            cout << stack << "\n";
+        }
+
+        if(s == "empty") {
+            if(stack == 0) 
+                cout << 1 << "\n";
+            else 
+                cout << 0 << "\n";
+        }
+
+        if(s == "top") {
+           cout << top() << "\n";
+        }
+    }
+}
--- boj1260.cpp
+++ boj1260.cpp
@@ -1,64 +1,64 @@
-// dfs와 bfs는 다뤄본 경험이 거의 없기 때문에 구글링함.
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <queue>
+// // dfs와 bfs는 다뤄본 경험이 거의 없기 때문에 구글링함.
+// #include <iostream>
+// #include <algorithm>
+// #include <vector>
+// #include <queue>
 
-using namespace std;
+// using namespace std;
 
-int N, M, V;
-vector<int>list[1001];
-bool visitedD[1001];
-bool visitedB[1001];
+// int N, M, V;
+// vector<int>list[1001];
+// bool visitedD[1001];
+// bool visitedB[1001];
 
-void dfs(int V) {
-    cout << V << " ";
-    visitedD[V] = true;
-    for(int i : list[V]) {
-        if(!visitedD[i]) {
-            dfs(i);
-        }
-    }
-}
+// void dfs(int V) {
+//     cout << V << " ";
+//     visitedD[V] = true;
+//     for(int i : list[V]) {
+//         if(!visitedD[i]) {
+//             dfs(i);
+//         }
+//     }
+// }
 
-void bfs(int V) {
-    queue<int> q; //큐 생성
-    q.push(V);    //시작노드 큐에 
-    visitedB[V] = true;
-    while(!q.empty()) {
-        int next = q.front();
-        q.pop();
-        cout << next << " ";
+// void bfs(int V) {
+//     queue<int> q; //큐 생성
+//     q.push(V);    //시작노드 큐에 
+//     visitedB[V] = true;
+//     while(!q.empty()) {
+//         int next = q.front();
+//         q.pop();
+//         cout << next << " ";
         
-        for(int i : list[next]) {
-            if(!visitedB[i]) {
-                q.push(i);
-                visitedB[i] = true;
-            }
-        }
-    }
-}
+//         for(int i : list[next]) {
+//             if(!visitedB[i]) {
+//                 q.push(i);
+//                 visitedB[i] = true;
+//             }
+//         }
+//     }
+// }
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
+// int main() {
+//     ios::sync_with_stdio(false);
+//     cin.tie(NULL);
 
-    cin >> N >> M >> V;
+//     cin >> N >> M >> V;
 
-    for(int i = 1; i <= M; i++) {
-        int u, v;
-        cin >> u >> v;
-        list[u].push_back(v);
-        list[v].push_back(u);
-    }
+//     for(int i = 1; i <= M; i++) {
+//         int u, v;
+//         cin >> u >> v;
+//         list[u].push_back(v);
+//         list[v].push_back(u);
+//     }
 
-    for(int i = 1; i <= N; i++) {
-        sort(list[i].begin(), list[i].end());
-    }
+//     for(int i = 1; i <= N; i++) {
+//         sort(list[i].begin(), list[i].end());
+//     }
 
-    dfs(V);
-    cout << "\n";
-    bfs(V);
+//     dfs(V);
+//     cout << "\n";
+//     bfs(V);
 
     
-}
+// }
