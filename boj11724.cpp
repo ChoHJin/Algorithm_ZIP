98d9c701be70e47d84ae1c2cb13aec6abb9cefba
ChoHJin <cjc9739@naver.com>
2022-08-05 23:30:18 +0900

    0805

---
+++ boj11724.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int>list[1001];
+bool visited[1001];
+
+void dfs(int v) {
+    visited[v] = true;
+
+    for (int i : list[v]) {
+        int next = list[v][i];
+        if(!visited[i]) {
+            dfs(i);
+        }
+    }
+}
+
+int main() {
+    int n, m;   //정점, 간선
+    int cnt = 0;
+
+    cin >> n >> m;
+
+    for(int i = 0; i < m; i++) {    //간선 연결
+        int u, v;
+        cin >> u >> v;
+        list[u].push_back(v);
+        list[v].push_back(u);
+    }
+
+    for(int i = 1; i <= n; i++) {
+        if(!visited[i]) {
+            cnt++;
+            dfs(i);
+        }
+    }
+
+    cout << cnt << endl;
+
+    return 0;
+}
--- boj2606.cpp
+++ boj2606.cpp
@@ -1,42 +1,42 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
 
-using namespace std;
+// using namespace std;
 
-bool visited[101];
-vector<int>com[101];
-int cnt = 0;
+// bool visited[101];
+// vector<int>com[101];
+// int cnt = 0;
 
 
-void dfs(int v) {
-    visited[v] = true;
-    cnt++;
+// void dfs(int v) {
+//     visited[v] = true;
+//     cnt++;
 
-    for (int i : com[v]) {
-        if(!visited[i]) {
-            dfs(i);
-        }
-    }
-}
+//     for (int i : com[v]) {
+//         if(!visited[i]) {
+//             dfs(i);
+//         }
+//     }
+// }
 
-int main() {
-    ios::sync_with_stdio(false);
-	cin.tie(NULL);
+// int main() {
+//     ios::sync_with_stdio(false);
+// 	cin.tie(NULL);
 
-    int n, p;
-    cin >> n;
-    cin >> p;
+//     int n, p;
+//     cin >> n;   //컴퓨터의 수
+//     cin >> p;   //연결되어 있는 컴퓨터 쌍의 수
 
-    for(int i = 0; i < p; i++) {
-        int c1, c2;
-        cin >> c1 >> c2;
-        com[c1].push_back(c2);
-        com[c2].push_back(c1);
-    }
-    dfs(1);
+//     for(int i = 0; i < p; i++) {
+//         int c1, c2;
+//         cin >> c1 >> c2;
+//         com[c1].push_back(c2);
+//         com[c2].push_back(c1);
+//     }
+//     dfs(1);
 
-    cout << cnt - 1 << "\n";
+//     cout << cnt - 1 << "\n";
 
-    return 0;
-}
+//     return 0;
+// }
