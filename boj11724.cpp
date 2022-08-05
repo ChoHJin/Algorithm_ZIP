7da742a80c3881215021fbd2d949bc5c8ccafd20
ChoHJin <cjc9739@naver.com>
2022-08-05 23:31:56 +0900

    0805 - 주석처리

--- boj11724.cpp
+++ boj11724.cpp
@@ -1,44 +1,44 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
 
-using namespace std;
+// using namespace std;
 
-vector<int>list[1001];
-bool visited[1001];
+// vector<int>list[1001];
+// bool visited[1001];
 
-void dfs(int v) {
-    visited[v] = true;
+// void dfs(int v) {
+//     visited[v] = true;
 
-    for (int i : list[v]) {
-        int next = list[v][i];
-        if(!visited[i]) {
-            dfs(i);
-        }
-    }
-}
+//     for (int i : list[v]) {
+//         int next = list[v][i];
+//         if(!visited[i]) {
+//             dfs(i);
+//         }
+//     }
+// }
 
-int main() {
-    int n, m;   //정점, 간선
-    int cnt = 0;
+// int main() {
+//     int n, m;   //정점, 간선
+//     int cnt = 0;
 
-    cin >> n >> m;
+//     cin >> n >> m;
 
-    for(int i = 0; i < m; i++) {    //간선 연결
-        int u, v;
-        cin >> u >> v;
-        list[u].push_back(v);
-        list[v].push_back(u);
-    }
+//     for(int i = 0; i < m; i++) {    //간선 연결
+//         int u, v;
+//         cin >> u >> v;
+//         list[u].push_back(v);
+//         list[v].push_back(u);
+//     }
 
-    for(int i = 1; i <= n; i++) {
-        if(!visited[i]) {
-            cnt++;
-            dfs(i);
-        }
-    }
+//     for(int i = 1; i <= n; i++) {
+//         if(!visited[i]) {
+//             cnt++;
+//             dfs(i);
+//         }
+//     }
 
-    cout << cnt << endl;
+//     cout << cnt << endl;
 
-    return 0;
-}
+//     return 0;
+// }
