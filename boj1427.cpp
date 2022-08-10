0641c7527be8967e56903bdd84393eaf78054f9e
ChoHJin <cjc9739@naver.com>
2022-08-10 23:05:31 +0900

    0810

---
+++ boj1427.cpp
@@ -0,0 +1,16 @@
+// #include <iostream>
+// #include <algorithm>
+// #include <string>
+
+// using namespace std;
+
+// int main() {
+
+//     string s;
+
+//     cin >> s;
+
+//     sort(s.begin(), s.end(), greater<>()); //greater은 내림차순으로 정렬해주는 sort 옵션
+
+//     cout << s << endl;
+// }
