4ec586c464c69e25589ed85853bedbb53b191baa
ChoHJin <cjc9739@naver.com>
2022-08-18 03:23:06 +0900

    0818 - 연습

---
+++ boj10807.cpp
@@ -0,0 +1,27 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     int n, f;
+//     int arr[101];
+//     int cnt = 0;
+
+//     cin >> n;
+
+//     for(int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
+
+//     cin >> f;
+
+//     for(int i = 0; i < n; i++) {
+//         if(arr[i] == f) {
+//             cnt++;
+//         }
+//     }
+
+//     cout << cnt << endl;
+
+//     return 0;
+// }
