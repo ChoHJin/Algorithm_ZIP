7b3e444e0847f4569641e54132c6c171b5443af6
ChoHJin <cjc9739@naver.com>
2022-08-21 00:09:33 +0900

    0821 - 새싹문제모음1

---
+++ boj15964.cpp
@@ -0,0 +1,16 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     long long a, b;
+    
+//     cin >> a >> b;
+
+//     cout << (a + b) * (a - b) << endl;
+
+//     return 0;
+// }
---
+++ boj2475.cpp
@@ -0,0 +1,21 @@
+// #include <iostream>
+// #include <cmath>
+
+// using namespace std;
+
+// int main() {
+//     ios::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n;
+//     int sum = 0;
+
+//     for(int i = 0; i < 5; i++) {
+//         cin >> n;
+//         sum += (n*n);
+//     }
+
+//     cout << sum << endl;
+
+//     return 0;
+// }
