5a60bced2d9da9dc878380a50c7938627b044262
ChoHJin <cjc9739@naver.com>
2022-08-21 00:18:02 +0900

    0821 - 새싹문제2

---
+++ boj10871.cpp
@@ -0,0 +1,26 @@
+// #include <iostream>
+
+// using namespace std;
+ 
+// int main() {
+//     ios::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n;
+//     int x = 0;
+//     int a[10001];
+    
+//     cin >> n >> x;
+
+//     for(int i = 0; i < n; i++) {
+//         cin >> a[i];
+//     }
+
+//     for(int i = 0; i < n; i++) {
+//         if(a[i] < x) {
+//             cout << a[i] << " ";
+//         }
+//     }
+
+    
+// }
