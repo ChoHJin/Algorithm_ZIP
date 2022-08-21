1b93184afdc2df7facc69c1625cfd6876d91a498
ChoHJin <cjc9739@naver.com>
2022-08-21 23:23:24 +0900

    0821 - 3

---
+++ boj2869.cpp
@@ -0,0 +1,20 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int cnt;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int a, b, v;
+
+//     cin >> a >> b >> v;
+
+//     cout << (v - b - 1) / (a - b) + 1 << endl;
+//     //달팽이가 하루 동안 가는 거리 : a - b
+//     //달팽이가 v-a를 가는 기간 : (v - a)/(a - b)
+//     //마지막날 달팽이는 떨어지기 전에 목표지점에 도착하기때문에 : v - b
+//     return 0;
+// }
