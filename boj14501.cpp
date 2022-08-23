10eaf2a83157a0d7a19ae2a1a0d51ac3c1dba8b2
ChoHJin <cjc9739@naver.com>
2022-08-23 23:15:57 +0900

    0823

--- boj14501.cpp
+++ boj14501.cpp
@@ -1,36 +1,36 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
 
-using namespace std;
+// using namespace std;
 
-int dp[16];
-pair<int, int> arr[16];
+// int dp[16];
+// pair<int, int> arr[16];
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
     
-    int n;
-    int profit = 0;
+//     int n;
+//     int profit = 0;
 
-    cin >> n;
+//     cin >> n;
 
-    for(int i = 0; i < n; i++) {
-        cin >> arr[i].first >> arr[i].second;
-    }
+//     for(int i = 0; i < n; i++) {
+//         cin >> arr[i].first >> arr[i].second;
+//     }
 
-    for(int i = 0; i <= n; i++) {
-        dp[i] = max(dp[i], profit);
+//     for(int i = 0; i <= n; i++) {
+//         dp[i] = max(dp[i], profit);
 
-        if(arr[i].first + i  <= n) {
-            dp[arr[i].first + i] = max(dp[arr[i].first + i], arr[i].second + dp[i]);
-        }
+//         if(arr[i].first + i  <= n) {
+//             dp[arr[i].first + i] = max(dp[arr[i].first + i], arr[i].second + dp[i]);
+//         }
 
-        profit = max(profit, dp[i]);
-    }
+//         profit = max(profit, dp[i]);
+//     }
 
-    cout << profit << endl;
+//     cout << profit << endl;
 
-    return 0;
-}
+//     return 0;
+// }
