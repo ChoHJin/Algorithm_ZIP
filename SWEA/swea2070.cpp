e3d69b40ad4701b6c7bffbca541e5d563a183fc1
ChoHJin <cjc9739@naver.com>
2022-08-27 15:30:27 +0900

    0827 - swea2

---
+++ SWEA/swea2070.cpp
@@ -0,0 +1,34 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main(int argc, char** argv) {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int test_case;
+//     int T;
+
+//     cin>>T;
+// /*
+// 여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+// */
+//     for(test_case = 1; test_case <= T; ++test_case) {
+
+//         int n, m;
+
+//         cin >> n >> m;
+
+//         if(n > m) {
+//             cout << "#" << test_case << " " << ">" << endl;
+//         }
+//         else if (n < m) {
+//             cout << "#" << test_case << " " << "<" << endl;
+//         }
+//         else {
+//             cout << "#" << test_case << " " << "=" << endl;
+//         }
+//     }
+
+//     return 0;//정상종료시 반드시 0을 리턴해야합니다.
+// }
