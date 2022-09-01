dc852953c786bc25a3031d47791994c469603099
ChoHJin <cjc9739@naver.com>
2022-09-01 22:56:42 +0900

    0901

---
+++ boj11650.cpp
@@ -0,0 +1,30 @@
+// #include <iostream>
+// #include <algorithm>
+// #include <vector>
+
+// using namespace std;
+
+// vector<pair<int, int>> map;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n;
+//     int x, y;
+
+//     cin >> n;    
+
+//     for(int i = 0; i < n; i++) {
+//         cin >> x >> y;
+//         map.push_back(make_pair(x, y));
+//     }
+
+//     sort(map.begin(), map.end());
+
+//     for(int i = 0; i < n; i++) {
+//         cout << map[i].first << " " << map[i].second << "\n";
+//     }
+
+//     return 0;
+// }
