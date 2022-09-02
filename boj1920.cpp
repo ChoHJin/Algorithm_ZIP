728bc19c754c6116917cf354333cfb03960268fd
ChoHJin <cjc9739@naver.com>
2022-09-03 00:02:06 +0900

    0902(3)__ binarysearch 추가

--- boj1920.cpp
+++ boj1920.cpp
@@ -4,13 +4,38 @@
 
 // using namespace std;
 
+// int n, m, num;
 // vector<int> v;
+
+// void binarySearch(int target) {
+//     int start = 0;
+//     int end = n - 1;
+//     int mid = 0;
+
+//     while(end >= start) {
+//         mid = (start + end) / 2;
+
+//         if(target == v[mid]) {
+//             cout << 1 << "\n";
+//             return;
+//         }
+
+//         else if (v[mid] < target) {
+//             start = mid + 1;
+//         }
+
+//         else {
+//             end = mid - 1;
+//         }
+//     }
+
+//     cout << 0 << "\n";
+//     return;
+// }
 
 // int main() {
 //     ios_base::sync_with_stdio(false);
 //     cin.tie(NULL);
-
-//     int n, m, num;
 
 //     cin >> n;
 
@@ -26,13 +51,14 @@
 
 //     for(int i = 0; i < m; i++) {
 //         cin >> num;
+//         // if(binary_search(v.begin(), v.end(), num)) {
+//         //     cout << 1 << "\n";
+//         // }
 
-//         if(binary_search(v.begin(), v.end(), num)) {
-//             cout << 1 << "\n";
-//         }
+//         // else 
+//         //     cout << 0 << "\n";
 
-//         else 
-//             cout << 0 << "\n";
+//         binarySearch(num);
 //     }
     
 //     return 0;
