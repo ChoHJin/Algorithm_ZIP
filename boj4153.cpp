de8201ec54c825edf05eceee92a64f8343ce2def
ChoHJin <cjc9739@naver.com>
2022-09-08 00:21:40 +0900

    0907(08일 자정)

---
+++ boj4153.cpp
@@ -0,0 +1,30 @@
+// #include <iostream>
+// #include <cmath>
+
+// using namespace std;
+
+// int a, b, c;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     while(1) {
+//         cin >> a >> b >> c;
+
+//         if(a == 0 && b == 0 && c == 0) {
+//             break;
+//         }
+
+//         else if(a*a == b*b + c*c || b*b == a*a + c*c || c*c == a*a + b*b) {
+//             cout << "right" << endl;
+//         }
+        
+//         else {
+//             cout << "wrong" << endl;
+//         } 
+        
+//     }
+
+//     return 0;
+// }
