5b0362e4dad3714edfddc94a6cc340c5e00eb780
ChoHJin <cjc9739@naver.com>
2022-09-09 03:25:35 +0900

    0908(9일새벽)

---
+++ boj11720.cpp
@@ -0,0 +1,23 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n, ans = 0;
+//     string s;
+
+//     cin >> n;
+
+//     cin >> s;
+
+//     for(int i = 0; i < n; i++) {
+//         ans += s[i] - '0';
+//     }
+
+//     cout << ans << endl;
+
+//     return 0;
+// }   
