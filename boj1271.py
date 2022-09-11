6554a5a8ff592caf0e22f6aa1b6c0e5541aa057b
ChoHJin <cjc9739@naver.com>
2022-09-11 23:50:41 +0900

    0911

--- boj1271.py
+++ boj1271.py
@@ -1,4 +1,4 @@
-n, m = map(int, input().split())
+# n, m = map(int, input().split())
 
-print(n // m)
-print(n % m)
+# print(n // m)
+# print(n % m)
---
+++ boj8958.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    int tmp = 0, ans = 0;
+    string s;
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+        cin >> s;
+
+        for(int j = 0; j < s.length(); j++) {
+            if(s[j] == 'O') {
+                tmp++;
+                ans += tmp;
+                
+            }
+            else if(s[j] == 'X') {
+                tmp = 0;
+            }
+        }
+
+        cout << ans << endl;
+        
+        tmp = 0;
+        ans = 0;
+    }
+
+    return 0;
+}
