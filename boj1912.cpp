0bb2036acc4c1114a1a2004ed149edf5db3441f9
ChoHJin <cjc9739@naver.com>
2022-09-12 15:59:14 +0900

    0912

--- boj1912.cpp
+++ boj1912.cpp
@@ -1,35 +1,35 @@
-#include <iostream>
-#include <algorithm>
+// #include <iostream>
+// #include <algorithm>
 
-using namespace std;
+// using namespace std;
 
-int arr[100001];
-int dp[100001];
+// int arr[100001];
+// int dp[100001];
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
 
-    int n;
-    int sum;
+//     int n;
+//     int sum;
 
-    cin >> n;
+//     cin >> n;
 
-    for(int i = 0; i < n; i++) {
-        cin >> arr[i];
-        dp[i] = arr[i];
-    }
+//     for(int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//         dp[i] = arr[i];
+//     }
 
-    sum = dp[0];
+//     sum = dp[0];
 
-    for(int i = 1; i < n; i++) {
-        dp[i] = max(dp[i], dp[i - 1] + arr[i]);
-        if(dp[i] > sum) {
-            sum = dp[i];
-        }
-    }
+//     for(int i = 1; i < n; i++) {
+//         dp[i] = max(dp[i], dp[i - 1] + arr[i]);
+//         if(dp[i] > sum) {
+//             sum = dp[i];
+//         }
+//     }
     
-    cout << sum << endl;
+//     cout << sum << endl;
 
-    return 0;
-}
+//     return 0;
+// }
