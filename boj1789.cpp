e86754b93a276e38129b7f16c1dddbbbde71280f
ChoHJin <cjc9739@naver.com>
2022-09-17 15:03:15 +0900

    0917

---
+++ boj1789.cpp
@@ -0,0 +1,31 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n = 1;
+//     int cnt = 0;
+//     long long s;
+//     long long sum = 0;
+
+//     cin >> s;
+
+//     while(1) {
+//         sum += n;
+//         cnt++;
+
+//         if(sum > s) {
+//             cnt--;
+//             break;
+//         }
+//         n++;
+//     }
+
+//     cout << cnt << endl;
+
+
+//     return 0;
+// }
