324376c742e1c5acd95c024ee13ac327e58c6cef
ChoHJin <cjc9739@naver.com>
2022-09-20 17:23:02 +0900

    0920. 백준 골드5 문제 아직 손대기엔 이르다고 판단해서 KEEP

---
+++ boj1107.cpp
@@ -0,0 +1,34 @@
+// #include <iostream>
+// #include <cstdlib>
+// #include <algorithm>
+
+// using namespace std;
+
+// bool errButton[10];    //고장난 버튼
+
+// int Possible(int n) {   //가능한 버튼인지 체크
+
+// }
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n, m, e;
+//     int num;
+//     int cc = 100;   //현재 채널
+
+//     cin >> n >> m;
+
+//     if(m != 0) {
+//         for(int i = 0; i < m; i++) {
+//             cin >> e;
+//             errButton[e] = true; 
+//         }
+//     }
+    
+
+    
+
+//     return 0;
+// }
