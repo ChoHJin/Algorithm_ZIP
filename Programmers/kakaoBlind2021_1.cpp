f9c05aa402405698646fa3861c6646308d27d7c2
ChoHJin <cjc9739@naver.com>
2022-09-20 16:34:05 +0900

    0920. 2021 카카오 코테 1번문제 1단계

---
+++ Programmers/kakaoBlind2021_1.cpp
@@ -0,0 +1,30 @@
+// #include <iostream>
+// #include <string>
+// #include <vector>
+
+// using namespace std;
+
+// string solution(string new_id) {
+//     string answer = "";
+    
+//     for(int i = 0; i < new_id.length(); i++) {
+//         if(isupper(new_id[i])) {    //1단계 풀이.
+//             answer += tolower(new_id[i]);
+//         }
+//     }
+    
+//     return answer;
+// }
+
+// int main() {
+//     ios::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     string s;
+
+//     cin >> s;
+
+//     cout << solution(s) << endl;
+
+//     return 0;
+// }
