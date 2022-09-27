1f6757330ad8cb95eb86baa83abfe7d6e06a0624
ChoHJin <cjc9739@naver.com>
2022-09-27 15:43:58 +0900

    0927-2

---
+++ boj2562.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n = 0, s = 0;
+    int arr[10] = {0, };
+
+    for(int i = 1; i <= 9; i++) {
+        cin >> arr[i];
+
+        if(n < arr[i]) {
+            n = arr[i];
+            s = i;
+        }
+    }
+
+    cout << n << "\n" << s << endl;
+
+
+    return 0;
+}
