6987fa0bb6151bdc9c73d98971b6ed021ae2bca6
ChoHJin <cjc9739@naver.com>
2022-09-27 16:22:57 +0900

    0927_3

--- boj2562.cpp
+++ boj2562.cpp
@@ -1,26 +1,26 @@
-#include <iostream>
-#include <algorithm>
+// #include <iostream>
+// #include <algorithm>
 
-using namespace std;
+// using namespace std;
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
 
-    int n = 0, s = 0;
-    int arr[10] = {0, };
+//     int n = 0, s = 0;
+//     int arr[10] = {0, };
 
-    for(int i = 1; i <= 9; i++) {
-        cin >> arr[i];
+//     for(int i = 1; i <= 9; i++) {
+//         cin >> arr[i];
 
-        if(n < arr[i]) {
-            n = arr[i];
-            s = i;
-        }
-    }
+//         if(n < arr[i]) {
+//             n = arr[i];
+//             s = i;
+//         }
+//     }
 
-    cout << n << "\n" << s << endl;
+//     cout << n << "\n" << s << endl;
 
 
-    return 0;
-}
+//     return 0;
+// }
---
+++ boj2920.cpp
@@ -0,0 +1,30 @@
+// #include <iostream>
+// #include <algorithm>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int arr[8] = {0, };
+//     int asc[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+//     int desc[8] = {8, 7, 6, 5, 4, 3, 2, 1};
+
+//     for(int i = 0; i < 8; i++) {
+//         cin >> arr[i];
+//     }
+
+//     if(equal(arr, arr+8, asc)) {
+//         cout << "ascending" << endl;
+//     }
+//     else if(equal(arr, arr+8, desc)) {
+//         cout << "descending" << endl;
+//     }
+//     else {
+//         cout << "mixed" << endl;
+//     }
+        
+
+//     return 0;
+// }
