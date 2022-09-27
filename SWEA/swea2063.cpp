65bfef4443a9b52e5d8b9d1df39d7ae3865ea4d6
ChoHJin <cjc9739@naver.com>
2022-09-27 16:42:07 +0900

    0927 _ swea 1

---
+++ SWEA/swea2063.cpp
@@ -0,0 +1,24 @@
+// #include <iostream>
+// #include <algorithm>
+
+// using namespace std;
+
+// int main(int argc, char** argv) {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n;
+//     int arr[200] = {0, };
+//     cin >> n;
+
+//     int mid = n / 2;
+
+//     for(int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
+//     sort(arr, arr+n);
+
+//     cout << arr[mid] << endl; 
+
+//     return 0;//정상종료시 반드시 0을 리턴해야합니다.
+// }
