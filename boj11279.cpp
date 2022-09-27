3ebd2316619b1f7ae74d721d92b8cfb0030075fd
ChoHJin <cjc9739@naver.com>
2022-09-27 14:47:36 +0900

    0927

---
+++ boj11279.cpp
@@ -0,0 +1,36 @@
+// #include <iostream>
+// #include <queue>
+// #include <stdio.h>
+
+// using namespace std;
+
+// priority_queue<int, vector<int>> pq;
+ 
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n, x;
+
+//     scanf("%d", &n);
+
+//     while(n--) {
+//         scanf("%d", &x);
+        
+//         if(x != 0) {
+//             pq.push(x);
+//         }
+//         else {            
+//             if(pq.empty()) {
+//                 printf("%d\n", x);
+//             }
+//             else {
+//                 printf("%d\n", pq.top());
+//                 pq.pop();
+//             }
+//         }
+//     }
+
+
+//     return 0;
+// }
