183e6f151a33b623a228156a479440177cc829b9
ChoHJin <cjc9739@naver.com>
2022-10-01 22:07:22 +0900

    1001

---
+++ Baekjoon/boj1676.cpp
@@ -0,0 +1,28 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n;
+//     int cnt = 0;
+
+//     cin >> n;
+
+//     // for(int i = 5; i <= n; i *= 5) {
+//     //     cnt += n / i;
+//     // }
+
+//     while(n >= 2) {
+//         if(n % 5 == 0) cnt++;
+//         if(n % 25 == 0) cnt++;
+//         if(n % 125 == 0) cnt++;
+//         n--;
+//     }
+
+//     cout << cnt << endl;
+
+//     return 0;
+// }
