da2c1bc80c081bde696fce44e787e4ea09f1ef07
ChoHJin <cjc9739@naver.com>
2022-10-16 23:31:45 +0900

    1016

---
+++ Baekjoon/boj2609.cpp
@@ -0,0 +1,30 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int euclid(int a, int b) {
+//     if(b == 0) {
+//         return a;
+//     }
+//     else 
+//         return euclid(b, a%b);
+// }
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n, m;
+//     int r = 0;
+//     int l = 0;
+
+//     cin >> n >> m;
+
+//     r = euclid(n, m);
+//     l = n * m / r;
+
+//     cout << r << endl;
+//     cout << l << endl;
+
+//     return 0;
+// }
