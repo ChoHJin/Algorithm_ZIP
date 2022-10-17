c3bfb546a4c47534bab0fd41085afef0f82166fe
ChoHJin <cjc9739@naver.com>
2022-10-17 18:03:49 +0900

    1017

---
+++ Baekjoon/boj2577.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int arr[11];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int a, b, c;
+
+    cin >> a >> b >> c;
+    int abc = a * b * c;
+
+    string res = to_string(abc);
+
+    for(int i = 0; i < res.size(); i++) {
+        for(int j = 0; j <= 9; j++) {
+            if(res[i] == (j + '0')) {
+                arr[j]++;
+            }
+        }
+    }
+
+    for(int i = 0; i <= 9; i++) {
+        cout << arr[i] << endl;
+    }
+
+    return 0;
+}
