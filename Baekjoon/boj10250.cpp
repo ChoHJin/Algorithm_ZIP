bebb647f7fe78400dc262db38339f47dbaf9b3b4
ChoHJin <cjc9739@naver.com>
2022-10-18 18:02:45 +0900

    1018_boj

---
+++ Baekjoon/boj10250.cpp
@@ -0,0 +1,30 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int w, h, n;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int T;
+//     int ans = 0;
+
+//     cin >> T;
+
+//     for(int i = 0; i < T; i++) {
+//         cin >> w >> h >> n;
+
+//         if(n % w == 0) {
+//             ans = w * 100 + (n / w);
+//         }
+//         else {
+//             ans = (n % w) * 100 + (n / w) + 1;
+//         }
+
+//         cout << ans << endl;
+//     }
+
+//     return 0;
+// }
---
+++ Baekjoon/boj11050.cpp
@@ -0,0 +1,29 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int factorial(int x) {
+//     int res = 1;
+
+//     for(int i = x; i > 0; i--){
+//         res *= i;
+//     }
+
+//     return res;
+// }
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n, m;
+//     int ans = 0;
+
+//     cin >> n >> m;
+
+//     ans = factorial(n) / (factorial(n - m) * factorial(m));
+
+//     cout << ans << "\n";
+
+//     return 0;
+// }
