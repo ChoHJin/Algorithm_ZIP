b3d6f2b673be985cd58b359c385051400e45a879
ChoHJin <cjc9739@naver.com>
2022-10-22 00:31:33 +0900

    1021

--- Baekjoon/boj1874.cpp
+++ Baekjoon/boj1874.cpp
@@ -1,10 +1,44 @@
-#include <iostream>
+// #include <iostream>
+// #include <stack>
+// #include <vector>
 
-using namespace std;
+// using namespace std;
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+// stack<int> seq;
+// vector<char> v;
 
-    return 0;
-}
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n;
+//     int cnt = 1;
+
+//     cin >> n;
+
+//     for(int i = 0; i < n; i++) {
+//         int num;
+//         cin >> num;
+        
+//         while(cnt <= num) {
+//             v.push_back('+');
+//             seq.push(cnt);
+//             cnt++;
+//         }
+
+//         if(num == seq.top()) {
+//             seq.pop();
+//             v.push_back('-');
+//         }
+//         else {
+//             cout << "NO";
+//             return 0;
+//         }
+//     }
+
+//     for(int i = 0; i < v.size(); i++) {
+//         cout << v[i] << "\n";
+//     }
+
+//     return 0;
+// }
