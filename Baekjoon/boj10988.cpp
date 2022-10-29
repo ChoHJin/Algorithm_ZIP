1297394b93fb2650aa984990ff65beeb4d65292f
ChoHJin <cjc9739@naver.com>
2022-10-29 23:40:22 +0900

    1029

---
+++ Baekjoon/boj10988.cpp
@@ -0,0 +1,27 @@
+// #include <iostream>
+// #include <algorithm>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     string s;
+//     string tmp;
+//     cin >> s;
+
+//     tmp = s;
+
+//     reverse(s.begin(), s.end());
+
+//     if(tmp == s) {
+//         cout << 1 << endl;
+//     }
+//     else {
+//         cout << 0 << endl;
+//     }
+
+
+//     return 0;
+// }
