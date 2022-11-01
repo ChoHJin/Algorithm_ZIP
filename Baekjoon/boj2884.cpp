52eef2901f34bd7ee9ec0c559b015d12f1482e6d
ChoHJin <cjc9739@naver.com>
2022-11-01 22:47:30 +0900

    1101_2

---
+++ Baekjoon/boj2884.cpp
@@ -0,0 +1,28 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int h, m;
+
+//     cin >> h >> m;
+    
+//     if(m - 45 < 0) {
+//         if(h - 1 < 0) {
+//             cout << 24 - 1 << " " << 60 + (m - 45); 
+//         }
+//         else
+//             cout << h - 1 << " " << 60 + (m - 45); 
+//     }
+//     else if (m - 45 == 0) {
+//         cout << h << " " << m - 45 << endl;
+//     }
+//     else {
+//         cout << h << " " << m - 45 << endl;
+//     }
+
+//     return 0;
+// }
