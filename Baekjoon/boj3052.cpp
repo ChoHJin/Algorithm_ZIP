0561ff1005e673525f771d4b30963679cd40dd36
ChoHJin <cjc9739@naver.com>
2022-11-01 22:34:36 +0900

    1101

---
+++ Baekjoon/boj3052.cpp
@@ -0,0 +1,28 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int arr[42];
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n;
+//     int cnt = 0;
+
+//     for(int i = 0; i < 10; i++) {
+//         cin >> n;
+//         arr[n % 42]++;
+//     }
+
+//     for(int i = 0; i < 42; i++) {
+//         if(arr[i] != 0) {
+//             cnt++;
+//         }
+//     }
+
+//     cout << cnt << endl;
+
+//     return 0;
+// }
