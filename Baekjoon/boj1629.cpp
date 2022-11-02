60fd155cb2b3ea2efe4873685b4cd97651e26d3f
ChoHJin <cjc9739@naver.com>
2022-11-02 23:47:38 +0900

    1102 _ 주석처리

--- Baekjoon/boj1629.cpp
+++ Baekjoon/boj1629.cpp
@@ -1,35 +1,35 @@
-#include <iostream>
+// #include <iostream>
 
-using namespace std;
+// using namespace std;
 
-long long POW(long long a, long long b, long long c) {
-    if(b == 0) return 1;
-    else {
-        long long tmp = POW(a, b / 2, c);
-        tmp = tmp*tmp % c;
+// long long POW(long long a, long long b, long long c) {
+//     if(b == 0) return 1;
+//     else {
+//         long long tmp = POW(a, b / 2, c);
+//         tmp = tmp*tmp % c;
         
-        if(b % 2 == 0) {
-            return tmp;
-        }
-        else {
-            return (tmp * a) % c;
-        }
-    }
-}
+//         if(b % 2 == 0) {
+//             return tmp;
+//         }
+//         else {
+//             return (tmp * a) % c;
+//         }
+//     }
+// }
 
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
 
-    int a, b, c;
-    int ans = 0;
+//     int a, b, c;
+//     int ans = 0;
 
-    cin >> a >> b >> c;
+//     cin >> a >> b >> c;
     
-    ans = POW(a, b, c);
+//     ans = POW(a, b, c);
 
-    cout << ans << endl;
+//     cout << ans << endl;
 
-    return 0;
-}
+//     return 0;
+// }
