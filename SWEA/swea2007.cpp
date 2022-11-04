26ebd1a481f59b0f58dc97961b63742c64acec1f
ChoHJin <cjc9739@naver.com>
2022-11-04 22:34:49 +0900

    1104

---
+++ SWEA/swea2007.cpp
@@ -0,0 +1,37 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main(int argc, char** argv) {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int test_case;
+//     int T;
+
+//     cin>>T;
+// /*
+// 여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+// */
+//     for(test_case = 1; test_case <= T; ++test_case) {
+//         string str;
+//         string token;
+
+//         cin >> str;
+//         token.push_back(str[0]);
+        
+//         for(int i = 1; i < str.size(); i++) {
+//             if(token != str.substr(token.size(), i)) {
+//                 token.push_back(str[i]);
+//             }
+//             else 
+//                 break;
+//         }
+
+
+//         cout << "#" << test_case << " ";
+//         cout << token.size() << endl;
+//     }
+
+//     return 0;//정상종료시 반드시 0을 리턴해야합니다.
+// }
