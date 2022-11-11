830b182df1c22e085b1a408a4bd6d633bab0c884
ChoHJin <cjc9739@naver.com>
2022-11-11 23:59:42 +0900

    1111

---
+++ Baekjoon/boj1547.cpp
@@ -0,0 +1,22 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n;
+//     int ans = 1;
+
+//     cin >> n;
+
+//     for(int i = 0; i < n; i++) {
+//         int a, b;
+//         cin >> a >> b;
+//         if(a == ans) ans = b;
+//         else if(b == ans) ans = a;
+//     }
+//     cout << ans << endl;
+//     return 0;
+// }
