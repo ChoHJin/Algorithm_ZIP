00238650d6e37667f402c81f2be07df207467520
ChoHJin <cjc9739@naver.com>
2022-11-12 23:53:17 +0900

    1112_boj_01타일

--- Baekjoon/boj1904.cpp
+++ Baekjoon/boj1904.cpp
@@ -1,26 +1,26 @@
-#include <iostream>
+// #include <iostream>
 
-using namespace std;
+// using namespace std;
 
-long long dp[1000001];
+// long long dp[1000001];
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
 
-    int n;
+//     int n;
 
-    cin >> n;
+//     cin >> n;
 
-    dp[0] = 0;
-    dp[1] = 1;
-    dp[2] = 2;
+//     dp[0] = 0;
+//     dp[1] = 1;
+//     dp[2] = 2;
 
-    for(int i = 3; i <= n; i++) {
-        dp[i] = (dp[i - 1] + dp[i - 2]) % 15746;
-    }
+//     for(int i = 3; i <= n; i++) {
+//         dp[i] = (dp[i - 1] + dp[i - 2]) % 15746;
+//     }
 
-    cout << dp[n] << endl;
+//     cout << dp[n] << endl;
 
-    return 0;
-}
+//     return 0;
+// }
