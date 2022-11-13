80c546c012b51e0aedacbc0314257e2bee4f371a
ChoHJin <cjc9739@naver.com>
2022-11-14 00:45:48 +0900

    1113

--- Baekjoon/boj1080.cpp
+++ Baekjoon/boj1080.cpp
@@ -1,60 +1,61 @@
-#include <iostream>
+// #include <iostream>
+// #include <cstdio>
 
-using namespace std;
+// using namespace std;
 
-int arrA[51][51];
-int arrB[51][51];
+// int arrA[51][51];
+// int arrB[51][51];
 
-void reverseArr(int y, int x) {
-    for(int i = y - 1; i <= y + 1; i++) {
-        for(int j = x - 1; j < x + 1; j++) {
-            arrA[i][j] = 1 - arrA[i][j];
-        }
-    }
-}
+// int n, m;
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+// bool reverseArr(int x, int y) {
+//     if(x + 3 > n || y + 3 > m) {
+//         return false;
+//     }
 
-    int n, m;
-    int cnt = 0;
+//     for(int i = x; i < x + 3; i++) {
+//         for(int j = y; j < y + 3; j++) {
+//             arrA[i][j] = !arrA[i][j];
+//         }
+//     }
+//     return true;
+// }
 
-    cin >> n >> m;
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
 
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < m; j++) {
-            // cin >> arrA[i][j];
-            scanf("%1d",&arrA[i][j]);
-        }
-    }
+//     int cnt = 0;
 
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < m; j++) {
-            // cin >> arrB[i][j];
-            scanf("%1d",&arrB[i][j]);
-        }
-    }
+//     scanf("%d %d", &n, &m);
 
-    for(int i = 0; i < n - 2; i++) {
-        for(int j = 0; j < m - 2; j++) {
-            if(arrA[i][j] != arrB[i][j]) {
-                cnt++;
-                reverseArr(i + 1, j + 1);
-            }
-        }
-    }
+//     for(int i = 0; i < n; i++) {
+//         for(int j = 0; j < m; j++) {
+//             scanf("%1d", &arrA[i][j]);
+//         }
+//     }
 
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < m; j++) {
-            if(arrA[i][j] != arrB[i][j]) {
-                cout << -1 << endl;
-                return 0;
-            }
-        }
-    }
+//     for(int i = 0; i < n; i++) {
+//         for(int j = 0; j < m; j++) {
+//             scanf("%1d", &arrB[i][j]);
+//         }
+//     }
 
-    cout << cnt << endl;
+//     for(int i = 0; i < n; i++) {
+//         for(int j = 0; j < m; j++) {
+//             if(arrA[i][j] != arrB[i][j]){
+//                 if(reverseArr(i, j)) {
+//                     cnt++;
+//                 }
+//                 else {
+//                     cout << -1 << endl;
+//                     return 0;
+//                 }
+//             }
+//         }
+//     }
 
-    return 0;
-}
+//     cout << cnt << endl;
+
+//     return 0;
+// }
