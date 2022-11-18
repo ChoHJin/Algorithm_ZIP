993e8345e3734b458cbfcae9a7749cb4e841e7e7
ChoHJin <cjc9739@naver.com>
2022-11-18 15:56:39 +0900

    1118_주석처리

--- Baekjoon/boj1068.cpp
+++ Baekjoon/boj1068.cpp
@@ -1,57 +1,57 @@
-#include <iostream>
-#include <vector>
+// #include <iostream>
+// #include <vector>
 
-using namespace std;
+// using namespace std;
 
-vector<int> tree[51];
-bool visited[51];
-int parent[51];
-int n, cnt, removeNode;
+// vector<int> tree[51];
+// bool visited[51];
+// int parent[51];
+// int n, cnt, removeNode;
 
-void dfs(int x) {
-    if(visited[x]) return;
+// void dfs(int x) {
+//     if(visited[x]) return;
 
-    visited[x] = true;
+//     visited[x] = true;
 
-    if(tree[x].size() == 0 || (tree[x].size() == 1 && tree[x][0] == removeNode)) {
-        cnt++;
-    }
+//     if(tree[x].size() == 0 || (tree[x].size() == 1 && tree[x][0] == removeNode)) {
+//         cnt++;
+//     }
 
-    for(int i = 0; i < tree[x].size(); i++) {
-        int next = tree[x][i];
+//     for(int i = 0; i < tree[x].size(); i++) {
+//         int next = tree[x][i];
 
-        if(removeNode != next) {
-            dfs(next);
-        }
-    }
-}
+//         if(removeNode != next) {
+//             dfs(next);
+//         }
+//     }
+// }
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
 
-    int root = -1;
+//     int root = -1;
 
-    cin >> n;
+//     cin >> n;
 
-    for(int i = 0; i < n; i++) {
-        int a;
-        cin >> a;
+//     for(int i = 0; i < n; i++) {
+//         int a;
+//         cin >> a;
 
-        if(a == -1) root = i;
-        else {
-            tree[a].push_back(i);
-        }
-    }
-    cin >> removeNode;
+//         if(a == -1) root = i;
+//         else {
+//             tree[a].push_back(i);
+//         }
+//     }
+//     cin >> removeNode;
 
-    if(removeNode == root) {
-        cout << 0 << endl;
-    }
-    else {
-        dfs(root);
-        cout << cnt << endl;
-    }
+//     if(removeNode == root) {
+//         cout << 0 << endl;
+//     }
+//     else {
+//         dfs(root);
+//         cout << cnt << endl;
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
