744eeed36ccb661254c605b5913f721cbe48efca
ChoHJin <cjc9739@naver.com>
2022-11-24 00:50:24 +0900

    1124

---
+++ Baekjoon/boj2292.cpp
@@ -0,0 +1,28 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n;
+//     int layer = 6;
+//     int cnt = 1;
+//     int ans = 1;
+
+//     cin >> n;
+    
+//     while(1) {
+//         if(cnt >= n) {
+//             break;
+//         }
+//         ans++;
+//         cnt += layer;
+//         layer += 6;
+//     }
+
+//     cout << ans << endl;
+
+//     return 0;
+// }
