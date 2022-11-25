41c68ab8c109c9588a940c12dd32a1254bc6fd25
ChoHJin <cjc9739@naver.com>
2022-11-26 02:30:10 +0900

    1126

---
+++ Baekjoon/boj2338.kt
@@ -0,0 +1,13 @@
+// import java.math.BigInteger
+
+// fun main() {
+//     val inputA = readLine()!!
+//     val inputB = readLine()!!
+
+//     val a = BigInteger(inputA)
+//     val b = BigInteger(inputB)
+
+//     println(a + b)
+//     println(a - b)
+//     println(a * b)
+// }
