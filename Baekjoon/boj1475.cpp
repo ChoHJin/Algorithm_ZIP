682e49c74f8814b0b4d4d8c08c2617a5daba4540
ChoHJin <cjc9739@naver.com>
2022-11-30 23:32:30 +0900

    1130

--- Baekjoon/boj1475.cpp
+++ Baekjoon/boj1475.cpp
@@ -1,37 +1,37 @@
-#include <iostream>
-#include <vector>
+// #include <iostream>
+// #include <vector>
 
-using namespace std;
+// using namespace std;
 
-vector<int> numSet(10);
+// vector<int> numSet(10);
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
 
-    string s;
-    int ans = 1;
+//     string s;
+//     int ans = 1;
 
-    cin >> s;
+//     cin >> s;
 
-    for(int i = 0; i < s.size(); i++) {
-        for(int j = 0; j < 10; j++) {
-            if(s[i] - '0' == j) {
-                numSet[j]++;
-            }
-        }
-    }
-    int num69 = (numSet[6] + numSet[9] + 1) / 2;
-    numSet[6] = num69;
-    numSet[9] = num69; 
+//     for(int i = 0; i < s.size(); i++) {
+//         for(int j = 0; j < 10; j++) {
+//             if(s[i] - '0' == j) {
+//                 numSet[j]++;
+//             }
+//         }
+//     }
+//     int num69 = (numSet[6] + numSet[9] + 1) / 2;
+//     numSet[6] = num69;
+//     numSet[9] = num69; 
 
-    for(int i = 0; i < numSet.size(); i++) {
-        if(ans < numSet[i]) {
-            ans = numSet[i];
-        }
-    }
+//     for(int i = 0; i < numSet.size(); i++) {
+//         if(ans < numSet[i]) {
+//             ans = numSet[i];
+//         }
+//     }
 
-    cout << ans << endl;
+//     cout << ans << endl;
 
-    return 0;
-}
+//     return 0;
+// }
