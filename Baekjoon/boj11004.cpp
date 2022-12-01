b14cff1fe079dc84ce91a7e763739d32d35044f8
ChoHJin <cjc9739@naver.com>
2022-12-01 15:08:02 +0900

    1201

---
+++ Baekjoon/boj11004.cpp
@@ -0,0 +1,29 @@
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+
+// using namespace std;
+
+// vector<long long> arr; 
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n, k;
+
+//     cin >> n >> k;
+
+//     arr.resize(n);
+
+//     for(int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
+
+//     sort(arr.begin(), arr.end());
+
+//     cout << arr[k - 1] << endl;
+
+
+//     return 0;
+// }
