e3c3f70ff96b4e9d52b1c97883a82aa6b77a7e3c
ChoHJin <cjc9739@naver.com>
2022-12-04 05:05:03 +0900

    1203

---
+++ Baekjoon/boj2693.cpp
@@ -0,0 +1,27 @@
+// #include <iostream>
+// #include <algorithm>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n;
+
+//     cin >> n;
+
+//     for(int i = 0; i < n; i++) {
+//         int arr[10];
+//         for(int j = 0; j < 10; j++) {
+//             cin >> arr[j];
+//         }
+
+//         sort(arr, arr + 10, greater<>());
+
+//         cout << arr[2] << endl;
+
+//     }
+
+//     return 0;
+// }
