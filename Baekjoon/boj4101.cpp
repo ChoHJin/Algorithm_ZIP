b340639777a618d5f88f2eba6ca1e42ab85ddc4b
ChoHJin <cjc9739@naver.com>
2022-12-04 20:07:33 +0900

    1204

---
+++ Baekjoon/boj4101.cpp
@@ -0,0 +1,26 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     while(1) {
+//         int a, b;
+
+//         cin >> a >> b;
+
+//         if(a == 0 && b == 0) {
+//             break;
+//         }
+
+//         if(a > b) {
+//             cout << "Yes" << endl;
+//         } else {
+//             cout << "No" << endl;
+//         }
+//     }
+
+//     return 0;
+// }
