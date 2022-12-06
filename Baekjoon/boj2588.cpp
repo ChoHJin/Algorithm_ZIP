553a42b5d1677f3c74408ce92856214b6f215cc8
ChoHJin <cjc9739@naver.com>
2022-12-06 23:57:55 +0900

    1206

--- Baekjoon/boj2588.cpp
+++ Baekjoon/boj2588.cpp
@@ -1,21 +1,21 @@
-#include <iostream>
+// #include <iostream>
 
-using namespace std;
+// using namespace std;
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
 
-    int a;
-    string b;
+//     int a;
+//     string b;
 
-    cin >> a >> b;
+//     cin >> a >> b;
 
-    cout << a * (b[2] - '0') << endl;
-    cout << a * (b[1] - '0') << endl;
-    cout << a * (b[0] - '0') << endl;
-    cout << a * stoi(b) << endl;
+//     cout << a * (b[2] - '0') << endl;
+//     cout << a * (b[1] - '0') << endl;
+//     cout << a * (b[0] - '0') << endl;
+//     cout << a * stoi(b) << endl;
     
 
-    return 0;
-}
+//     return 0;
+// }
