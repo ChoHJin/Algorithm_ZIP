7d6f6089a8d2e49d7593ca7ae2356264871a8fac
ChoHJin <cjc9739@naver.com>
2022-12-07 23:25:56 +0900

    1207

---
+++ Baekjoon/boj2161.cpp
@@ -0,0 +1,28 @@
+// #include <iostream>
+// #include <queue>
+
+// using namespace std;
+
+// queue<int> q;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n;
+
+//     cin >> n;
+
+//     for(int i = 1; i <= n; i++) {
+//         q.push(i);
+//     }
+
+//     while(!q.empty()) {
+//         cout << q.front() << " ";
+//         q.pop();
+//         q.push(q.front());
+//         q.pop();
+//     }
+
+//     return 0;
+// }
