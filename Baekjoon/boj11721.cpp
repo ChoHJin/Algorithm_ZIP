e6fe3fa510347ba26a9d105c71fc26f26be27555
ChoHJin <cjc9739@naver.com>
2022-12-09 23:12:04 +0900

    1209

---
+++ Baekjoon/boj11721.cpp
@@ -0,0 +1,26 @@
+// #include <iostream>
+// #include <string>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     string s;
+//     int cnt = 0;
+
+//     getline(cin, s);
+    
+//     for(int i = 0; i < s.size(); i++) {
+//         cnt++;    
+//         cout << s[i];
+
+//         if(cnt == 10) {
+//             cout << "\n";
+//             cnt = 0;
+//         }
+//     }
+
+//     return 0;
+// }
