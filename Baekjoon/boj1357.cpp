3d2733c147b6da26dc29bec6278aaa55f6b65d17
ChoHJin <cjc9739@naver.com>
2022-12-11 22:09:05 +0900

    1211

---
+++ Baekjoon/boj1357.cpp
@@ -0,0 +1,29 @@
+// #include <iostream>
+// #include <algorithm>
+// #include <string>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int x, y;
+
+//     cin >> x >> y;
+
+//     string rX, rY;
+//     rX = to_string(x);
+//     rY = to_string(y);
+
+//     reverse(rX.begin(), rX.end());
+//     reverse(rY.begin(), rY.end());
+
+//     string ans = to_string(stoi(rX) + stoi(rY));
+
+//     reverse(ans.begin(), ans.end());
+
+//     cout << stoi(ans) << endl;
+
+//     return 0;
+// }
