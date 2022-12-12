c5b29967ccbb47dcba7fda523a049bbbfc5feaea
ChoHJin <cjc9739@naver.com>
2022-12-12 19:35:50 +0900

    1212

---
+++ Baekjoon/boj2490.cpp
@@ -0,0 +1,27 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int yoot, sum;
+
+//     for(int i = 0; i < 3; i++) {
+//         sum = 0;
+
+//         for(int j = 0; j < 4; j++) {
+//             cin >> yoot;
+//             sum += yoot;
+//         }
+        
+//         if(sum == 0) cout << "D" << endl;
+//         else if (sum == 1) cout << "C" << endl;
+//         else if (sum == 2) cout << "B" << endl;
+//         else if (sum == 3) cout << "A" << endl;
+//         else if (sum == 4) cout << "E" << endl;
+//     }
+
+//     return 0;
+// }
