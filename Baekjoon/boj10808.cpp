34b2675cd7c69d95e650498a45f5634f20537761
ChoHJin <cjc9739@naver.com>
2022-12-15 23:47:42 +0900

    1215

---
+++ Baekjoon/boj10808.cpp
@@ -0,0 +1,25 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int alpha[26];
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     string s;
+
+//     cin >> s;
+
+//     for(int i = 0; i < s.size(); i++) {
+//         alpha[s[i] - 97]++;
+//     }
+
+//     for(int i = 0; i < 26; i++) {
+//         cout << alpha[i] << " "; 
+//     }
+//     cout << endl;
+
+//     return 0;
+// }
