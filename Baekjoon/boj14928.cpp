a087b3073c93b1f2228a38e8ea7666450048250c
ChoHJin <cjc9739@naver.com>
2022-12-16 23:57:30 +0900

    1216

---
+++ Baekjoon/boj14928.cpp
@@ -0,0 +1,24 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     string s;
+//     int ans = 0;
+
+//     cin >> s;
+
+//     for(int i = 0; i < s.size(); i++) {
+//         int d = s[i] - '0';
+
+//         ans = ((ans * 10) % 20000303 + d) % 20000303;
+//     }
+
+//     cout << ans << endl;
+
+
+//     return 0;
+// }
