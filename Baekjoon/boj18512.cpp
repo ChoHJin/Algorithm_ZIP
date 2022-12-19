988c9dcbcb04c073c785c07d577bd5eaeda162de
ChoHJin <cjc9739@naver.com>
2022-12-20 00:09:19 +0900

    1219_ 수정

--- Baekjoon/boj18512.cpp
+++ Baekjoon/boj18512.cpp
@@ -1,32 +1,45 @@
 // #include <iostream>
+// #include <vector>
 
 // using namespace std;
+
+// vector<int> a;
+// vector<int> b;
 
 // int main() {
 //     ios_base::sync_with_stdio(false);
 //     cin.tie(NULL);
 
 //     int x, y, p1, p2;
-
+    
 //     cin >> x >> y >> p1 >> p2;
 
+//     a.push_back(p1);
+//     b.push_back(p2);
+
 //     while(1) {
-//         if(p1 > p2) {
-//             p2 += y;
+//         int ans = 0;
+//         a.push_back(p1 += x);
+//         b.push_back(p2 += y);
+
+//         for(int i = 0; i < a.size(); i++) {
+//             for(int j = 0; j < b.size(); j++) {
+//                 if(a[i] == b[j]) {
+//                     ans = a[i];
+//                     break;
+//                 }
+//             }
 //         }
-//         else if (p1 < p2) {
-//             p1 += x;
-//         }
-//         else {
-//             cout << p1 << endl;
+
+//         if(ans != 0) {
+//             cout << ans << endl;
 //             break;
 //         }
 
-//         if(p1 > 10000 || p2 > 10000) {
+//         if(a.size() == 1000) { 
 //             cout << -1 << endl;
 //             break;
 //         }
-
 //     }
 
 //     return 0;
