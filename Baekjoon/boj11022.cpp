81a6555414e1bbf0850a334358525b051a219cab
ChoHJin <cjc9739@naver.com>
2022-12-22 00:09:25 +0900

    1221

---
+++ Baekjoon/boj11022.cpp
@@ -0,0 +1,22 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n;
+//     int a, b;
+//     int sum = 0;
+
+//     cin >> n;
+
+//     for(int i = 0; i < n; i++) {
+//         cin >> a >> b;
+//         sum = a + b;
+//         cout << "Case #" << i + 1 << ": " << a << " + " << b << " = " << sum << endl;
+//     }
+
+//     return 0;
+// }
