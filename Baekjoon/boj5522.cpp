f5c6c621b5bbb7d324d07b2874bb21ce7d5d704e
ChoHJin <cjc9739@naver.com>
2022-12-24 22:28:13 +0900

    1224

---
+++ Baekjoon/boj5522.cpp
@@ -0,0 +1,21 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int sum = 0;
+
+//     for(int i = 0; i < 5; i++) {
+//         int n;
+//         cin >> n;
+
+//         sum += n;
+//     }
+
+//     cout << sum << endl;
+
+//     return 0;
+// }
