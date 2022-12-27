6bfa36420126756f7b906b995d6b72341cb8964b
ChoHJin <cjc9739@naver.com>
2022-12-27 23:18:40 +0900

    1227

---
+++ Baekjoon/boj2752.cpp
@@ -0,0 +1,27 @@
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+    
+//     int n;
+//     vector<int> v;
+
+//     for(int i = 0; i < 3; i++) {
+//         cin >> n;
+
+//         v.push_back(n);
+//     }
+
+//     sort(v.begin(), v.end());
+
+//     for(int i = 0; i < v.size(); i++) {
+//         cout << v[i] << " ";
+//     }
+
+//     return 0;
+// }
