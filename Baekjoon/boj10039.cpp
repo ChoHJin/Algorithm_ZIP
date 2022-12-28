5e19ab03f37ed966ba89a58175c7f257e6167941
ChoHJin <cjc9739@naver.com>
2022-12-28 23:32:16 +0900

    1228

---
+++ Baekjoon/boj10039.cpp
@@ -0,0 +1,24 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int arr[5];
+//     int ans = 0;
+
+//     for(int i = 0; i < 5; i++) {
+//         cin >> arr[i];
+
+//         if(arr[i] < 40) {
+//             ans += 40;
+//         }
+//         else ans += arr[i];
+//     }
+
+//     cout << ans / 5 << endl;
+
+//     return 0;
+// }
