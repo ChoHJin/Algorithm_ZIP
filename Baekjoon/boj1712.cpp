ed972c1efd4ec613acd135aea128c3234f00d0ed
ChoHJin <cjc9739@naver.com>
2022-12-30 21:04:30 +0900

    1230

---
+++ Baekjoon/boj1712.cpp
@@ -0,0 +1,22 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int a, b, c;
+
+//     cin >> a >> b >> c;
+
+//     if(b >= c) {
+//         cout << -1 << endl;
+//     }
+//     else {
+//         cout << a / (c - b) + 1;
+//     }
+
+
+//     return 0;
+// }
