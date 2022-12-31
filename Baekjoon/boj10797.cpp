ea5fd5812d503d257b11f150025e1138ab5151c8
ChoHJin <cjc9739@naver.com>
2022-12-31 23:50:14 +0900

    1231_2022_fin

---
+++ Baekjoon/boj10797.cpp
@@ -0,0 +1,22 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int five[5] = {0, };
+//     int n, cnt = 0;
+
+//     cin >> n;
+
+//     for(int i = 0; i < 5; i++) {
+//         cin >> five[i];
+//         if(five[i] == n) cnt++;
+//     }
+
+//     cout << cnt << endl;
+
+//     return 0;
+// }
