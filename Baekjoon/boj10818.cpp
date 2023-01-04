c0ef45e18a69684466f488dbef49bca3141dec27
ChoHJin <cjc9739@naver.com>
2023-01-04 23:14:21 +0900

    0104

---
+++ Baekjoon/boj10818.cpp
@@ -0,0 +1,29 @@
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+
+// using namespace std;
+
+// vector<int> v;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n;
+
+//     cin >> n;
+
+//     for(int i = 0; i < n; i++) {
+//         int a;
+//         cin >> a;
+
+//         v.push_back(a);
+//     }
+
+//     sort(v.begin(), v.end());
+
+//     cout << v.front() << " " << v.back() << endl;
+
+//     return 0;
+// }
---
+++ Baekjoon/boj5086.cpp
@@ -0,0 +1,31 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+    
+//     int a, b;
+
+//     while(1) {
+//         cin >> a >> b;
+
+//         if(a == 0 && b == 0) {
+//             break;
+//         }
+
+//         if(a % b == 0) {
+//             cout << "multiple" << endl;
+//         }
+//         else if(b % a == 0) {
+//             cout << "factor" << endl;
+//         }
+//         else {
+//             cout << "neither" << endl;
+//         }
+
+//     }
+
+//     return 0;
+// }
