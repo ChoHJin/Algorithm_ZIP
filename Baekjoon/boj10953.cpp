4bd99a3d5154c35f2ae1f056fec63dec41652f95
ChoHJin <cjc9739@naver.com>
2023-01-06 19:57:02 +0900

    0106

---
+++ Baekjoon/boj10953.cpp
@@ -0,0 +1,30 @@
+// #include <iostream>
+// #include <string>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n;
+
+//     cin >> n;
+
+//     for(int i = 0; i < n; i++) {
+//         string s = "";
+
+//         // getline(cin, s);
+//         cin >> s;
+
+//         for(int j = 0; j < s.size(); j++) {
+//             if(s[j] == ',') {
+//                 cout << (s[j - 1] - '0') + (s[j + 1] - '0') << endl;
+//                 break;
+//             }
+//         }
+//     }
+    
+
+//     return 0;
+// }
