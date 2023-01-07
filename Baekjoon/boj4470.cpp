f53c581ee8e13bb564c6aa9cd05354811e394219
ChoHJin <cjc9739@naver.com>
2023-01-08 00:07:46 +0900

    0107_늦음

---
+++ Baekjoon/boj4470.cpp
@@ -0,0 +1,23 @@
+// #include <iostream>
+// #include <string>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n;
+//     string s;
+
+//     cin >> n;
+//     cin.ignore();
+
+//     for(int i = 1; i <= n; i++) {
+//         getline(cin, s);
+
+//         cout << i << ". " << s << endl;
+//     }
+
+//     return 0;
+// }
