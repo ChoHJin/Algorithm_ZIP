ac100fceb0053787eb89ac0efed415eb3887f4a2
ChoHJin <cjc9739@naver.com>
2023-01-12 22:11:31 +0900

    0112

---
+++ Baekjoon/boj10870.cpp
@@ -0,0 +1,26 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int dp[1000];
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n;
+
+//     cin >> n;
+
+//     dp[0] = 0;
+//     dp[1] = 1;
+
+//     for(int i = 2; i <= n; i++) {
+//         dp[i] = dp[i - 1] + dp[i - 2];
+//     }
+
+//     cout << dp[n] << endl;
+    
+
+//     return 0;
+// }
