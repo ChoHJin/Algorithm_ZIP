571390a9cd608f683b0347dd90a3f3d190222239
ChoHJin <cjc9739@naver.com>
2023-01-15 04:09:16 +0900

    0114

---
+++ Baekjoon/boj10101.cpp
@@ -0,0 +1,30 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int a, b, c;
+
+//     cin >> a >> b >> c;
+
+//     if(a == 60 && b == 60 && c == 60) {
+//         cout << "Equilateral" << endl;
+//     }
+//     else if(a + b + c == 180) {
+//         if(a == b || a == c || b == c) {
+//             cout << "Isosceles" << endl;
+//         }
+        
+//         if(a != b && a != c && b != c) {
+//             cout << "Scalene" << endl;
+//         }
+//     }
+//     else if(a + b + c != 180) {
+//         cout << "Error" << endl;
+//     }
+
+//     return 0;
+// }
