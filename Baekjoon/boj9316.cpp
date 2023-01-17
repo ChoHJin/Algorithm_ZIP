b15768cc4b70bf26bcba65452e230a3233a99f1c
ChoHJin <cjc9739@naver.com>
2023-01-17 22:48:56 +0900

    0117

---
+++ Baekjoon/boj9316.cpp
@@ -0,0 +1,19 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     string s = "Hello World, Judge ";
+
+//     int n;
+//     cin >> n;
+
+//     for(int i = 1; i <= n; i++) {
+//         cout << s << i << "!" << endl;
+//     }
+
+//     return 0;
+// }
