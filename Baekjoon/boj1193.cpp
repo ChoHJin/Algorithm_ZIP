6eeba5b3dd0d0990509b7625cb491bf6836bd58b
ChoHJin <cjc9739@naver.com>
2023-01-21 00:45:29 +0900

    0120

---
+++ Baekjoon/boj1193.cpp
@@ -0,0 +1,28 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n;
+
+//     cin >> n;
+
+//     int cnt = 1;
+
+//     while(n > cnt) {
+//         n -= cnt;
+//         cnt++;
+//     }
+
+//     if(cnt % 2 == 1) {
+//         cout << cnt + 1 - n << "/" << n << endl;
+//     }
+//     else {
+//         cout << n << "/" << cnt + 1 - n << endl;
+//     }
+
+//     return 0;
+// }
--- Baekjoon/boj7785.cpp
+++ Baekjoon/boj7785.cpp
@@ -1,31 +1,31 @@
-#include <iostream>
-#include <map>
+// #include <iostream>
+// #include <map>
 
-using namespace std;
+// using namespace std;
 
-map<string, string, greater<>> m;
+// map<string, string, greater<>> m;
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
 
-    int n;
-    string name;
-    string access;
+//     int n;
+//     string name;
+//     string access;
 
-    cin >> n;
+//     cin >> n;
 
-    for(int i = 0; i < n; i++) {
-        cin >> name >> access;
-        m[name] = access;
-    }
+//     for(int i = 0; i < n; i++) {
+//         cin >> name >> access;
+//         m[name] = access;
+//     }
     
-    map<string, string>::iterator it;
-    for(it = m.begin(); it!=m.end(); it++){
-        if(it->second == "enter"){
-            cout << it->first << "\n";
-        }
-    }
+//     map<string, string>::iterator it;
+//     for(it = m.begin(); it!=m.end(); it++){
+//         if(it->second == "enter"){
+//             cout << it->first << "\n";
+//         }
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
