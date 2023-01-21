4f9e757ad32c71659db70dabe9f359271d7f6842
ChoHJin <cjc9739@naver.com>
2023-01-21 19:08:29 +0900

    0121

---
+++ Baekjoon/boj1297.cpp
@@ -0,0 +1,21 @@
+// #include <iostream>
+// #include <cmath>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int d, h, w;
+
+//     cin >> d >> h >> w;
+
+//     double a = sqrt(w * w + h * h);
+//     double b = w * d / a;
+//     double c = h * d / a;
+
+//     cout << (int)c << " " << (int)b << endl;
+
+//     return 0;
+// }
