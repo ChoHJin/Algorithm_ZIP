214f6d39be9485e7399948d37dc70f6ef4409b89
ChoHJin <cjc9739@naver.com>
2023-01-23 22:18:52 +0900

    0123

---
+++ Baekjoon/boj4999.cpp
@@ -0,0 +1,16 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     string j, d;
+
+//     cin >> j >> d;
+
+//     if(j.length() >= d.length()) cout << "go" << endl;
+//     else cout << "no" << endl;
+//     return 0;
+// }
