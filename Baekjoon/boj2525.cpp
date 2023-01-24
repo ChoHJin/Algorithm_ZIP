03df4f235ef3fafdd1243b5620b8050cb81c956e
ChoHJin <cjc9739@naver.com>
2023-01-24 23:21:49 +0900

    0124

---
+++ Baekjoon/boj2525.cpp
@@ -0,0 +1,23 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int a, b, c;
+
+//     cin >> a >> b >> c;
+
+//     int m = 60 * a + b;
+
+//     m += c;
+
+//     int h = (m / 60) % 24;
+//     int n = m % 60;
+
+//     cout << h << " " << n;
+
+//     return 0;
+// }
