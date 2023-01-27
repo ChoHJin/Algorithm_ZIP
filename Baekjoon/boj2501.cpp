a30ccf350b12ed2fbf6cfa4eddfe70b7222e02da
ChoHJin <cjc9739@naver.com>
2023-01-27 22:36:46 +0900

    0127

---
+++ Baekjoon/boj2501.cpp
@@ -0,0 +1,29 @@
+// #include <iostream>
+// #include <vector>
+
+// using namespace std;
+
+// vector<int> ans;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n, k;
+//     int cnt = 0;
+
+//     cin >> n >> k;
+
+//     for(int i = 1; i <= n; i++) {
+//         if(n % i == 0) {
+//             ans.push_back(i);
+//             cnt++;
+//         }
+//     }
+
+//     if(cnt < k) cout << 0 << endl;
+//     else cout << ans[k - 1] << endl;
+
+
+//     return 0;
+// }
