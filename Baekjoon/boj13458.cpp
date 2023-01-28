549af8add3b7e69eeb04a3f74a8b508445395127
ChoHJin <cjc9739@naver.com>
2023-01-28 19:13:47 +0900

    0128

--- Baekjoon/boj13458.cpp
+++ Baekjoon/boj13458.cpp
@@ -1,39 +1,39 @@
-#include <iostream>
-#include <vector>
+// #include <iostream>
+// #include <vector>
 
-using namespace std;
+// using namespace std;
 
-vector<int> v;
+// vector<int> v;
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
 
-    int n, b, c;
+//     int n, b, c;
 
-    cin >> n;
+//     cin >> n;
 
-    for(int i = 0; i < n; i++) {
-        int a;
-        cin >> a;
-        v.push_back(a);
-    }
+//     for(int i = 0; i < n; i++) {
+//         int a;
+//         cin >> a;
+//         v.push_back(a);
+//     }
 
-    cin >> b >> c;
+//     cin >> b >> c;
 
-    long long ans = 0;
+//     long long ans = 0;
 
-    for(int i = 0; i < n; i++) {
-        if(v[i] <= b) {
-            ans++;
-        }
-        else {
-            ans += (v[i] - b) / c + 1;
-            if((v[i] - b) % c != 0) ans++;
-        }
-    }
+//     for(int i = 0; i < n; i++) {
+//         if(v[i] <= b) {
+//             ans++;
+//         }
+//         else {
+//             ans += (v[i] - b) / c + 1;
+//             if((v[i] - b) % c != 0) ans++;
+//         }
+//     }
 
-    cout << ans << endl;
+//     cout << ans << endl;
 
-    return 0;
-}
+//     return 0;
+// }
