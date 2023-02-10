91629ed6e85aaee7083bf427f213c4ce7af54165
ChoHJin <cjc9739@naver.com>
2023-02-10 23:37:04 +0900

    0210

---
+++ Baekjoon/boj10093.cpp
@@ -0,0 +1,36 @@
+// #include <iostream>
+// #include <vector>
+
+// using namespace std;
+
+// vector<long long> v;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     long long a, b;
+//     long long tmp = 0;
+
+//     cin >> a >> b;
+
+//     if(a > b) {
+//         tmp = a;
+//         a = b;
+//         b = tmp;
+//     }
+
+//     for(long long i = a + 1; i < b; i++) {
+//         v.push_back(i);
+//     }
+
+//     cout << v.size() << endl;
+
+//     for(int i = 0; i < v.size(); i++) {
+//         cout << v[i] << " ";
+//     }
+//     cout << endl;
+
+
+//     return 0;
+// }
