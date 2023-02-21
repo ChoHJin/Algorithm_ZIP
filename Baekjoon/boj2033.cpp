ef55bf593641c1306bd7b9c21604d225c88b7850
ChoHJin <cjc9739@naver.com>
2023-02-21 22:38:36 +0900

    0221

---
+++ Baekjoon/boj2033.cpp
@@ -0,0 +1,27 @@
+// #include <iostream>
+// #include <cmath>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n, m = 10;
+//     int tmp;
+
+//     cin >> n;
+
+//     while(n > m) {
+//         tmp = n % m;
+//         n -= tmp;
+
+//         if(tmp >= 5 * (m / 10)) n += m;
+
+//         m *= 10; 
+//     }
+
+//     cout << n;
+
+//     return 0;
+// }
