d5f5fc6e24d4be31920ec29d5118c50b5a41970c
ChoHJin <cjc9739@naver.com>
2023-02-25 15:57:58 +0900

    0225

---
+++ Baekjoon/boj10886.cpp
@@ -0,0 +1,26 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n;
+//     int vote, cute = 0, notCute = 0;
+
+//     cin >> n;
+
+//     while(n--) {
+//         cin >> vote;
+
+//         if(!vote) {
+//             notCute++;
+//         } else cute++;
+//     }
+
+//     if(cute > notCute) cout << "Junhee is cute!" << endl;
+//     else cout << "Junhee is not cute!" << endl;
+
+//     return 0;
+// }
