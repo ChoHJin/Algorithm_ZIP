5293541e94e85cdc7abf2a95d819ad8ad6f1c83f
ChoHJin <cjc9739@naver.com>
2023-02-26 23:51:17 +0900

    0226

---
+++ Baekjoon/boj6996.cpp
@@ -0,0 +1,32 @@
+// #include <iostream>
+// #include <algorithm>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+    
+//     int n;
+
+//     cin >> n;
+
+//     while(n--) {
+//         string a, b, tmpA, tmpB;
+
+//         cin >> a >> b;
+
+//         tmpA = a;
+//         tmpB = b;
+
+//         sort(tmpA.begin(), tmpA.end());
+//         sort(tmpB.begin(), tmpB.end());
+
+//         if(tmpA == tmpB) {
+//             cout << a << " & " << b << " are anagrams." << endl; 
+//         } else cout << a << " & " << b << " are NOT anagrams." << endl;
+
+//     }
+
+//     return 0;
+// }
