806422fc0c79a33464e8f5959dc0f037b44d4525
ChoHJin <cjc9739@naver.com>
2023-03-01 23:46:45 +0900

    0301

---
+++ Baekjoon/boj27434.kt
@@ -0,0 +1,18 @@
+// package com.tutorials.test2
+
+// import java.math.BigInteger
+
+// fun factorial(n: Int): BigInteger {
+//     if (n == 0 || n == 1) {
+//         return BigInteger.ONE
+//     }
+//     return BigInteger.valueOf(n.toLong()) * factorial(n - 1)
+// }
+
+// fun main() {
+//     val n = readLine()!!.toInt()
+
+//     val ans = factorial(n)
+
+//     print(ans)
+// }
