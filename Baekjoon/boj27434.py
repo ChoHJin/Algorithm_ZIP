b6775f60b8d59c7c742111c895554bfceacb8162
ChoHJin <cjc9739@naver.com>
2023-03-04 23:24:11 +0900

    0303

---
+++ Baekjoon/boj27434.py
@@ -0,0 +1,12 @@
+// import math
+// import sys
+
+// def factorial(n):
+//     if n == 0 or n == 1:
+//         return 1
+//     return n * factorial(n - 1)
+
+// if __name__ == "__main__":
+//     n = int(sys.stdin.readline().strip())
+//     ans = factorial(n)
+//     print(ans)
