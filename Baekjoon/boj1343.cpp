d2b891ffb260692fc347d6cbea532be890d6af16
ChoHJin <cjc9739@naver.com>
2023-03-09 23:10:58 +0900

    0309

---
+++ Baekjoon/boj1343.cpp
@@ -0,0 +1,23 @@
+// #include <iostream>
+// #include <regex>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     string s;
+//     cin >> s;
+
+//     s = regex_replace(s, regex("XXXX"), "AAAA");
+//     s = regex_replace(s, regex("XX"), "BB");
+
+//     if(s.find("X") != -1) 
+//         cout << -1 << endl;
+//     else 
+//         cout << s << endl;
+
+
+//     return 0;
+// }
