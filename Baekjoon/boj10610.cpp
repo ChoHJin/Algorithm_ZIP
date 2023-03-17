aa289861abfde5494fde43f35838b0807617666f
ChoHJin <cjc9739@naver.com>
2023-03-17 23:54:19 +0900

    0317_주석처리

--- Baekjoon/boj10610.cpp
+++ Baekjoon/boj10610.cpp
@@ -1,30 +1,30 @@
-#include <iostream>
-#include <algorithm>
+// #include <iostream>
+// #include <algorithm>
 
-using namespace std;
+// using namespace std;
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
 
-    string n;
-    cin >> n;
+//     string n;
+//     cin >> n;
     
-    int sum = 0;
-    bool isZeroExist = false;
-    for (char c : n) {
-        if (c == '0') isZeroExist = true;
-        sum += c - '0';
-    }
+//     int sum = 0;
+//     bool isZeroExist = false;
+//     for (char c : n) {
+//         if (c == '0') isZeroExist = true;
+//         sum += c - '0';
+//     }
 
-    if (!isZeroExist || sum % 3 != 0) {
-        cout << "-1\n";
-        return 0;
-    }
+//     if (!isZeroExist || sum % 3 != 0) {
+//         cout << "-1\n";
+//         return 0;
+//     }
 
-    sort(n.begin(), n.end(), greater<char>());
-    cout << n << "\n";
+//     sort(n.begin(), n.end(), greater<char>());
+//     cout << n << "\n";
 
 
-    return 0;
-}
+//     return 0;
+// }
