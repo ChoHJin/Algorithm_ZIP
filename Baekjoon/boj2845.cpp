78d835676301533dbbfe73081343159f0ca84c35
ChoHJin <cjc9739@naver.com>
2023-03-21 23:46:15 +0900

    0321

---
+++ Baekjoon/boj2845.cpp
@@ -0,0 +1,23 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int l, p, n;
+
+//     cin >> l >> p;
+
+//     int people = l * p;
+
+//     for(int i = 0; i < 5; i++) {
+//         cin >> n;
+//         cout << n - people << " ";
+//     }
+
+//     cout << endl;
+
+//     return 0;
+// }
