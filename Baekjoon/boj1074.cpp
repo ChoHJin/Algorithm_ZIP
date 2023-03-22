271287eb8e86cdb8c53e5f1191c7870ab9da3c26
ChoHJin <cjc9739@naver.com>
2023-03-22 17:37:23 +0900

    0322

--- Baekjoon/boj1074.cpp
+++ Baekjoon/boj1074.cpp
@@ -1,45 +1,45 @@
-#include <iostream>
-#include <cmath>
+// #include <iostream>
+// #include <cmath>
 
-using namespace std;
+// using namespace std;
 
-int N, c, r, answer;
+// int N, c, r, answer;
 
-void board(int n, int x, int y) {
-    if(n == 1) {
-        if(x == c && y == r) {
-            cout << answer << endl;
-        }
-        answer++;
-        return;
-    }
+// void board(int n, int x, int y) {
+//     if(n == 1) {
+//         if(x == c && y == r) {
+//             cout << answer << endl;
+//         }
+//         answer++;
+//         return;
+//     }
 
-    int half = n / 2;
+//     int half = n / 2;
 
-    if(c < x + half && r < y + half) {
-        board(half, x, y);
-    }
-    else if(c < x + half && r >= y + half) {
-        answer += half * half;
-        board(half, x, y + half);
-    }
-    else if(c >= x + half && r < y + half) {
-        answer += half * half * 2;      //pow를 사용시 값이 double형으로 반환되어 결과값이 다르게 나옴
-        board(half, x + half, y);
-    }
-    else {
-        answer += half * half * 3;
-        board(half, x + half, y + half);
-    }
-}
+//     if(c < x + half && r < y + half) {
+//         board(half, x, y);
+//     }
+//     else if(c < x + half && r >= y + half) {
+//         answer += half * half;
+//         board(half, x, y + half);
+//     }
+//     else if(c >= x + half && r < y + half) {
+//         answer += half * half * 2;      //pow를 사용시 값이 double형으로 반환되어 결과값이 다르게 나옴
+//         board(half, x + half, y);
+//     }
+//     else {
+//         answer += half * half * 3;
+//         board(half, x + half, y + half);
+//     }
+// }
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
 
-    cin >> N >> c >> r;
-    int size = pow(2, N);
-    board(size, 0, 0);
+//     cin >> N >> c >> r;
+//     int size = pow(2, N);
+//     board(size, 0, 0);
 
-    return 0;
-}
+//     return 0;
+// }
