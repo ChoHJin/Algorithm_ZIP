e458e5bef9b7e83f6c5b3473463e7d7e4155ebd4
ChoHJin <cjc9739@naver.com>
2023-03-23 22:23:36 +0900

    0323

---
+++ Baekjoon/boj2083.cpp
@@ -0,0 +1,25 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+    
+//     string name;
+//     int a, w;
+
+//     while(1) {
+//         cin >> name >> a >> w;
+
+//         if(name == "#" && a == 0 && w == 0) break;
+
+//         if(a > 17 || w >= 80) {
+//             cout << name << " " << "Senior" << '\n';
+//         } else {
+//             cout << name << " " << "Junior" << '\n';
+//         }
+//     }    
+
+//     return 0;
+// }
