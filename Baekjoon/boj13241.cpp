e8575f797d77013b196a7318990437e5880e8caa
ChoHJin <cjc9739@naver.com>
2023-03-24 23:04:48 +0900

    0324

---
+++ Baekjoon/boj13241.cpp
@@ -0,0 +1,28 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int gcd(int a, int b) {
+//     if(b == 0) {
+//         return a;
+//     } else {
+//         return gcd(b, a % b);
+//     }
+    
+// }
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     long long a, b;
+
+//     cin >> a >> b;
+
+//     long long g = gcd(a, b);
+//     long long l = a * b / g;
+
+//     cout << l << endl;
+
+//     return 0;
+// }
