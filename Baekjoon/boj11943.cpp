f8d4f150027700de8980183e714aaed773eb6a8d
ChoHJin <cjc9739@naver.com>
2023-03-31 03:27:44 +0900

    0330_Late sub

---
+++ Baekjoon/boj11943.cpp
@@ -0,0 +1,17 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int a, b, c, d;
+//     cin >> a >> b >> c >> d;
+
+//     int ans = b + c < a + d ? b + c : a + d;
+
+//     cout << ans << endl;
+
+//     return 0;
+// }
