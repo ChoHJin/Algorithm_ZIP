4f3d1f113e56859121a3d6865e9f5e3a5753c6a8
ChoHJin <cjc9739@naver.com>
2023-03-31 23:46:49 +0900

    0331

---
+++ Baekjoon/boj2455.cpp
@@ -0,0 +1,22 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int cur = 0, ans = 0;
+
+//     for(int i = 0; i < 4; i++) {
+//         int a, b;
+//         cin >> a >> b;
+
+//         cur += b - a;
+//         ans = max(ans, cur);
+//     }
+
+//     cout << ans << endl;
+
+//     return 0;
+// }
