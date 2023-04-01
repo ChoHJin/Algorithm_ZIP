03f3b9c3dee9904377f43acf8952eb3078d0dd57
ChoHJin <cjc9739@naver.com>
2023-04-01 23:40:42 +0900

    0401

---
+++ Baekjoon/boj2566.cpp
@@ -0,0 +1,28 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int arr[10][10];
+//     int maxVal = -1, maxRow, maxCol;
+
+//     for(int i=0;i<9;i++){
+//         for(int j=0;j<9;j++){
+//             cin >> arr[i][j];
+//             if(arr[i][j]>maxVal){
+//                 maxVal = arr[i][j];
+//                 maxRow = i+1;
+//                 maxCol = j+1;
+//             }
+//         }
+//     }
+
+//     cout << maxVal << '\n';
+//     cout << maxRow << ' ' << maxCol << '\n';
+
+
+//     return 0;
+// }
