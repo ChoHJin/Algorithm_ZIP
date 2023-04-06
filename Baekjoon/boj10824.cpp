844df8c7b66972feca3a6b905a8e2489d55e6296
ChoHJin <cjc9739@naver.com>
2023-04-06 23:52:47 +0900

    0406

---
+++ Baekjoon/boj10824.cpp
@@ -0,0 +1,21 @@
+// #include <iostream>
+// #include <string>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     string a, b, c, d;
+
+//     cin >> a >> b >> c >> d;
+
+//     long long ans = 0;
+
+//     ans = stoll(a + b) + stoll(c + d); 
+
+//     cout << ans << endl;
+
+//     return 0;
+// }
