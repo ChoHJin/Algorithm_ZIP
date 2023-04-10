68e49a837673ed1516b071522d0fd9cdb15b58ba
ChoHJin <cjc9739@naver.com>
2023-04-10 23:55:38 +0900

    0410(주석처리)

--- Baekjoon/boj15873.cpp
+++ Baekjoon/boj15873.cpp
@@ -1,20 +1,20 @@
-#include <iostream>
-#include <string>
+// #include <iostream>
+// #include <string>
 
-using namespace std;
+// using namespace std;
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
 
-    string s;
-    cin >> s;
+//     string s;
+//     cin >> s;
 
-    int a = stoi(s.length() >= 3 && s[1] == '0' ? s.substr(0, 2) : s.substr(0, 1));
-	int b = stoi(a == 10 ? s.substr(2) : s.substr(1));
-	int ans = a + b;
+//     int a = stoi(s.length() >= 3 && s[1] == '0' ? s.substr(0, 2) : s.substr(0, 1));
+// 	int b = stoi(a == 10 ? s.substr(2) : s.substr(1));
+// 	int ans = a + b;
 
-	cout << ans << endl;
+// 	cout << ans << endl;
 
-    return 0;
-}
+//     return 0;
+// }
