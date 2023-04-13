dfa406104415440408f8b15fd1765cf5cab00674
ChoHJin <cjc9739@naver.com>
2023-04-13 23:41:16 +0900

    0413

---
+++ Baekjoon/boj1057.cpp
@@ -0,0 +1,21 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n, a, b, cnt = 0;
+
+//     cin >> n >> a >> b;
+
+//     while (a != b) {
+//         a = (a + 1) / 2;
+//         b = (b + 1) / 2;
+//         cnt++;
+//     }
+
+//     cout << cnt << endl;
+//     return 0;
+// }
