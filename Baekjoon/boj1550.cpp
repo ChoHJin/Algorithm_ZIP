9edf552440e5279e9939f5a5ba36dd95cee836e4
ChoHJin <cjc9739@naver.com>
2023-04-16 23:49:17 +0900

    0416

---
+++ Baekjoon/boj1550.cpp
@@ -0,0 +1,14 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);  
+
+//     int n;
+//     cin >> hex >> n;  // 16진수로 입력받음
+//     cout << n << endl;
+
+//     return 0;
+// }
