920f1db38c51f609640f862267c451acaf385083
ChoHJin <cjc9739@naver.com>
2023-05-06 16:06:26 +0900

    0506

---
+++ Baekjoon/boj25305.cpp
@@ -0,0 +1,27 @@
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+
+// using namespace std;
+
+// vector<int> v;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n, k;
+//     cin >> n >> k;
+
+//     for(int i = 0; i < n; i++) {
+//         int num;
+//         cin >> num;
+//         v.push_back(num);
+//     }
+
+//     sort(v.begin(), v.end(), greater<>());
+
+//     cout << v[k - 1] << endl;
+
+//     return 0;
+// }
