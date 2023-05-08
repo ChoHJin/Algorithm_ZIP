db3bd02af88e2c88b9151a89d7e807e73340f509
ChoHJin <cjc9739@naver.com>
2023-05-08 23:25:27 +0900

    0508

---
+++ Baekjoon/boj27866.cpp
@@ -0,0 +1,16 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     string s;
+//     int idx;
+//     cin >> s >> idx;
+
+//     cout << s[idx - 1] << endl;    
+
+//     return 0;
+// }
