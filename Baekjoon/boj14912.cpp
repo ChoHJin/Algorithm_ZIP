a893300f9d86495ced6ae9d24ebee63b8eb54950
ChoHJin <cjc9739@naver.com>
2023-05-14 23:18:43 +0900

    0514

---
+++ Baekjoon/boj14912.cpp
@@ -0,0 +1,26 @@
+// #include <iostream>
+// #include <string>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n, d, ans = 0;
+//     cin >> n >> d;
+
+//     for(int i = 1; i <= n; i++) {
+//         string s = to_string(i);
+
+//         for(int j = 0; j < s.size(); j++) {
+//             if(s[j] - '0' == d) {
+//                 ans++;
+//             }
+//         }
+//     }
+
+//     cout << ans << endl;
+
+//     return 0;
+// }
