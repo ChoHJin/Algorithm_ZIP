44fb930efaa323882e2ab9755078e80507dd7d52
ChoHJin <cjc9739@naver.com>
2023-05-15 23:43:19 +0900

    주석처리

--- Baekjoon/boj1145.cpp
+++ Baekjoon/boj1145.cpp
@@ -1,34 +1,34 @@
-#include <iostream>
+// #include <iostream>
 
-using namespace std;
+// using namespace std;
 
-int arr[5];
+// int arr[5];
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
 
-    for(int i = 0; i < 5; i++) {
-        cin >> arr[i];
-    }
+//     for(int i = 0; i < 5; i++) {
+//         cin >> arr[i];
+//     }
 
-    int ans = 1;
+//     int ans = 1;
 
-    while(1) {
-        int cnt = 0;
+//     while(1) {
+//         int cnt = 0;
 
-        for(int i = 0; i < 5; i++) {
-            if(ans % arr[i] == 0) {
-                cnt++;
-            }
-        }
+//         for(int i = 0; i < 5; i++) {
+//             if(ans % arr[i] == 0) {
+//                 cnt++;
+//             }
+//         }
 
-        if(cnt >= 3) {
-            cout << ans << endl;
-            break;
-        }
-        ans++;
-    }
+//         if(cnt >= 3) {
+//             cout << ans << endl;
+//             break;
+//         }
+//         ans++;
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
