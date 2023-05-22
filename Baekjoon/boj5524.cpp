af2c453225cf05174499ee04b0dcb3b0c4db8991
ChoHJin <cjc9739@naver.com>
2023-05-22 22:37:45 +0900

    0522

---
+++ Baekjoon/boj5524.cpp
@@ -0,0 +1,27 @@
+// #include <iostream>
+// #include <string>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n;
+//     cin >> n;
+
+//     while(n--) {
+//         string s;
+//         cin >> s;
+
+//         for(int i = 0; i < s.size(); i++) {
+//             if(s[i] >= 'A' && s[i] <= 'Z') {
+//                 s[i] += 32;
+//             }
+//         }
+
+//         cout << s << endl;
+//     }
+
+//     return 0;
+// }
