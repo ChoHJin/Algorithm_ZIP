e761b750f1909f955ac5f68fac054a9ede8a12da
ChoHJin <cjc9739@naver.com>
2023-05-25 21:30:18 +0900

    0525

---
+++ Baekjoon/boj13866.cpp
@@ -0,0 +1,26 @@
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+// #include <cmath>
+
+// using namespace std;
+
+// vector<int> v(4);
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+    
+//     for(int i = 0; i < 4; i++) {
+//         cin >> v[i];
+//     }
+
+//     sort(v.begin(), v.end());
+
+//     int ans = 0;
+//     ans = (v[0] + v[3]) - (v[1] + v[2]);
+
+//     cout << abs(ans) << endl;
+
+//     return 0;
+// }
