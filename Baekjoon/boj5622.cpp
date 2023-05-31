0849d033b13a0ee49d6a03552407773317e80a82
ChoHJin <cjc9739@naver.com>
2023-05-31 22:57:53 +0900

    더 좋은 방법 추가

--- Baekjoon/boj5622.cpp
+++ Baekjoon/boj5622.cpp
@@ -42,3 +42,27 @@
 
 //     return 0;
 // }
+
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     string dial;
+//     cin >> dial;
+
+//     int time[26] = {3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 10, 10, 10, 10};
+
+//     int ans = 0;
+
+//     for(int i = 0; i < dial.size(); i++) {
+//         ans += time[dial[i] - '65'];
+//     }
+
+//     cout << ans << endl;
+
+//     return 0;
+//  }
