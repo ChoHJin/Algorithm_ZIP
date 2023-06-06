7a618c967334ee3e7a9297b7c845b8a990b9ee3e
ChoHJin <cjc9739@naver.com>
2023-06-06 15:34:52 +0900

    0605_ 주석처리

--- Baekjoon/boj2559.cpp
+++ Baekjoon/boj2559.cpp
@@ -1,40 +1,40 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
 
-using namespace std;
+// using namespace std;
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
 
-    int n, k;
+//     int n, k;
 
-    cin >> n >> k;
+//     cin >> n >> k;
 
-    vector<int> v(n);
+//     vector<int> v(n);
 
-    for(int i = 0; i < n; i++) {
-        cin >> v[i];
-    }
+//     for(int i = 0; i < n; i++) {
+//         cin >> v[i];
+//     }
 
-    int sum = 0;
-    int ans = -1e9;
+//     int sum = 0;
+//     int ans = -1e9;
 
-    for(int i = 0; i < k; i++) {
-        sum += v[i];
-    }
+//     for(int i = 0; i < k; i++) {
+//         sum += v[i];
+//     }
 
-    ans = sum;
+//     ans = sum;
 
-    for(int i = k; i < n; i++) {
-        sum += v[i] - v[i - k];
+//     for(int i = k; i < n; i++) {
+//         sum += v[i] - v[i - k];
 
-        ans = max(ans, sum);
-    }
+//         ans = max(ans, sum);
+//     }
 
 
-    cout << ans << '\n';
+//     cout << ans << '\n';
 
-    return 0;
-}
+//     return 0;
+// }
