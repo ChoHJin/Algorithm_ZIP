6c1055571e3d605af77a375e0c9617fd4d51d3a5
ChoHJin <cjc9739@naver.com>
2023-06-06 15:45:39 +0900

    0606

---
+++ Baekjoon/boj10972.cpp
@@ -0,0 +1,33 @@
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+
+// using namespace std;
+
+// vector<int> v;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n;
+
+//     cin >> n;
+
+//     for(int i = 0; i < n; i++) {
+//         int num;
+//         cin >> num;
+
+//         v.push_back(num);
+//     }
+
+//     if(next_permutation(v.begin(), v.end())) {
+//         for(int i = 0; i < v.size(); i++) {
+//             cout << v[i] << " ";
+//         }
+//     } else {
+//         cout << -1 << endl;
+//     }
+
+//     return 0;
+// }
