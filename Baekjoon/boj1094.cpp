29e92684b6d86411bc2b047f89ab5da9e9dd8aa1
ChoHJin <cjc9739@naver.com>
2023-07-01 15:11:17 +0900

    1094 문제 비트마스킹 코드 추가

--- Baekjoon/boj1094.cpp
+++ Baekjoon/boj1094.cpp
@@ -27,3 +27,30 @@
 
 //     return 0;
 // }
+
+//비트마스킹
+
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int x;
+//     cin >> x;
+
+//     int cnt = 0;
+
+//     while(x > 0) {
+//         if(x & 1) { //x의 가장 오른쪽 비트가 1인 경우
+//             cnt++;  
+//         }
+//         x >>= 1;    //x를 오른쪽으로 한 비트씩 이동
+//     }
+
+//     cout << cnt << endl;
+
+//     return 0;
+// }
