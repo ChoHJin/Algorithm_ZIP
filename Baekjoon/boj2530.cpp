f59ba88fb42e19e42c45ca82a5a2feee947a8495
ChoHJin <cjc9739@naver.com>
2023-07-03 17:23:45 +0900

    0703

---
+++ Baekjoon/boj2530.cpp
@@ -0,0 +1,28 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int A, B, C;
+//     cin >> A >> B >> C;
+
+//     int D;
+//     cin >> D;
+
+//     int totalSeconds = A * 3600 + B * 60 + C + D;
+
+//     int hours = totalSeconds / 3600;
+//     totalSeconds %= 3600;
+
+//     int minutes = totalSeconds / 60;
+//     totalSeconds %= 60;
+
+//     int seconds = totalSeconds;
+
+//     cout << hours % 24 << " " << minutes << " " << seconds << endl;
+
+//     return 0;
+// }
