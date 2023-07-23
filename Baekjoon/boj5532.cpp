1d878dfabda9c82c4cbcb67407e0919b0af2a74f
ChoHJin <cjc9739@naver.com>
2023-07-23 23:36:31 +0900

    0723

---
+++ Baekjoon/boj5532.cpp
@@ -0,0 +1,21 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int l, a, b, c, d;
+//     cin >> l >> a >> b >> c >> d;
+
+//     int k = (b + d - 1) / d;
+//     int m = (a + c - 1) / c;
+
+//     int maxDay = max(k, m);
+//     int ans = l - maxDay;
+
+//     cout << ans << endl;
+
+//     return 0;
+// }
