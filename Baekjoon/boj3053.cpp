d456abd10a3b2c44370a8a17f16b86a2b735ae48
ChoHJin <cjc9739@naver.com>
2023-07-29 22:11:07 +0900

    0729

---
+++ Baekjoon/boj3053.cpp
@@ -0,0 +1,20 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     double n;
+//     cin >> n;
+
+//     double PI = 3.1415926535897932;
+    
+//     cout << fixed;
+//     cout.precision(6);
+//     cout << n * n * PI << endl;
+//     cout << 2 * n * n << endl;
+
+//     return 0;
+// }
