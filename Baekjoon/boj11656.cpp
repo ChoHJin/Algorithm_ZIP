c0d50568de59ae7349bfdcd1333df8d3d2090601
ChoHJin <cjc9739@naver.com>
2023-08-10 22:34:32 +0900

    0810

---
+++ Baekjoon/boj11656.cpp
@@ -0,0 +1,29 @@
+// #include <iostream>
+// #include <string>
+// #include <algorithm>
+// #include <vector>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     string s;
+//     cin >> s;
+
+//     vector<string> v;
+
+//     for(int i = 0; i < s.length(); i++) {
+//         v.push_back(s.substr(i));
+//     }
+
+//     sort(v.begin(), v.end());
+
+//     for(string &it : v) {
+//         cout << it << '\n';
+//     }
+
+
+//     return 0;
+// }
