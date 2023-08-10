42da3e9b61acf74c93957c3b08da588ba495adc9
ChoHJin <cjc9739@naver.com>
2023-08-10 22:30:10 +0900

    0809 주석처리

--- Baekjoon/boj28438.cpp
+++ Baekjoon/boj28438.cpp
@@ -1,35 +1,35 @@
-#include <iostream>
-#include <vector>
+// #include <iostream>
+// #include <vector>
 
-using namespace std;
+// using namespace std;
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
 
-    int n, m, q;
-    cin >> n >> m >> q;
+//     int n, m, q;
+//     cin >> n >> m >> q;
 
-    vector<int> r(n);
-    vector<int> c(m);
+//     vector<int> r(n);
+//     vector<int> c(m);
 
-    for(int i = 0; i < q; i++) {
-        int op, a, b;
-        cin >> op >> a >> b;
+//     for(int i = 0; i < q; i++) {
+//         int op, a, b;
+//         cin >> op >> a >> b;
 
-        if(op == 1) {
-            r[a - 1] += b;
-        } else {
-            c[a - 1] += b;
-        }
-    }
+//         if(op == 1) {
+//             r[a - 1] += b;
+//         } else {
+//             c[a - 1] += b;
+//         }
+//     }
 
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < m; j++) {
-            cout << r[i] + c[j] << ' ';
-        }
-        cout << '\n';
-    }
+//     for(int i = 0; i < n; i++) {
+//         for(int j = 0; j < m; j++) {
+//             cout << r[i] + c[j] << ' ';
+//         }
+//         cout << '\n';
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
