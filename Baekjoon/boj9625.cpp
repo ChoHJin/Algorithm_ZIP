80d4a0ddd21e89e69eedcaa1ff69edb60eee5b5e
ChoHJin <cjc9739@naver.com>
2023-08-24 23:45:33 +0900

    0824

---
+++ Baekjoon/boj9625.cpp
@@ -0,0 +1,26 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n;
+//     cin >> n;
+
+//     int a = 1;
+//     int b = 0;
+
+//     for(int i = 0; i < n; i++) {
+//         int tmpA = b;
+//         int tmpB = a + b;
+
+//         a = tmpA;
+//         b = tmpB;
+//     }
+
+//     cout << a << " " << b << endl;
+    
+//     return 0;
+// }
