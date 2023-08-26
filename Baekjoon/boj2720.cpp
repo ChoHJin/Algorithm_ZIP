47bbf9946f857d9aed21e40ffba0f3fb670061c8
ChoHJin <cjc9739@naver.com>
2023-08-26 23:28:21 +0900

    0826

---
+++ Baekjoon/boj2720.cpp
@@ -0,0 +1,31 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int t;
+//     cin >> t;
+
+//     while(t--) {
+//         int c;
+//         cin >> c;
+
+//         int q = c / 25;
+//         c %= 25;
+
+//         int d = c / 10;
+//         c %= 10;
+
+//         int n = c / 5;
+//         c %= 5;
+
+//         int p = c;
+
+//         cout << q << " " << d << " " << n << " " << p << endl;
+//     }
+
+//     return 0;
+// }
