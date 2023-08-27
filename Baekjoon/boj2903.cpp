e14e0ae4823f594b9cf00da8b9fe720fd2fcdff9
ChoHJin <cjc9739@naver.com>
2023-08-27 23:45:11 +0900

    0827

---
+++ Baekjoon/boj2903.cpp
@@ -0,0 +1,22 @@
+// #include <iostream>
+// #include <cmath>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n;
+//     cin >> n;
+
+//     int p = 2;
+
+//     while(n--) {
+//         p = p * 2 - 1;
+//     }
+
+//     cout << p * p << endl;
+
+//     return 0;
+// }
