126f131837181ebd414daeb9f534ea8c4ae26275
ChoHJin <cjc9739@naver.com>
2023-09-03 22:01:48 +0900

    0903

---
+++ Baekjoon/boj10768.cpp
@@ -0,0 +1,25 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int m, d;
+//     cin >> m >> d;
+
+//     if(m == 2) {
+//         if(d == 18) {
+//             cout <<  "Special" << endl;
+//         } else if(d > 18) {
+//             cout << "After" << endl;
+//         } else cout << "Before" << endl;
+//     } else if(m > 2) {
+//         cout << "After" << endl;
+//     } else {
+//         cout << "Before" << endl;
+//     }
+
+//     return 0;
+// }
