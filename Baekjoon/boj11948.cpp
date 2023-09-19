cb5020e94a2dfd6bb6a6cb0e598fb1639a022b7e
ChoHJin <cjc9739@naver.com>
2023-09-19 09:52:00 +0900

    0919

---
+++ Baekjoon/boj11948.cpp
@@ -0,0 +1,34 @@
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     vector<int> v1;
+//     vector<int> v2;
+
+//     for(int i = 0; i < 4; i++) {
+//         int n;
+//         cin >> n;
+
+//         v1.push_back(n);
+//     }
+
+//     for(int i = 0; i < 2; i++) {
+//         int n;
+//         cin >> n;
+
+//         v2.push_back(n);
+//     }
+
+//     sort(v1.begin(), v1.end());
+//     sort(v2.begin(), v2.end());
+
+//     cout << v1[3] + v1[2] + v1[1] + v2[1] << endl;
+
+//     return 0;
+// }
