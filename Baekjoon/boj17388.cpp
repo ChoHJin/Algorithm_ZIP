f39d18c49c5aba5ff70cecd9a8a798bf64add936
ChoHJin <cjc9739@naver.com>
2023-09-21 21:42:11 +0900

    0921

---
+++ Baekjoon/boj17388.cpp
@@ -0,0 +1,25 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int s, k, h;
+//     cin >> s >> k >> h;
+
+//     if(s + k + h >= 100) {
+//         cout << "OK" << endl;
+//     } else {
+//         if(s < k && s < h) {
+//             cout << "Soongsil" << endl;
+//         } else if(k < s && k < h) {
+//             cout << "Korea" << endl;
+//         } else if(h < s && h < k) {
+//             cout << "Hanyang" << endl;
+//         }
+//     }
+
+//     return 0;
+// }
