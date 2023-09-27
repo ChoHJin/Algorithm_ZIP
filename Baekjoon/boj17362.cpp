be6d597a7c6d54e3796f2430ff2f995bf90559ef
ChoHJin <cjc9739@naver.com>
2023-09-27 23:11:32 +0900

    0927

---
+++ Baekjoon/boj17362.cpp
@@ -0,0 +1,23 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n;
+//     cin >> n;
+
+//     if(n % 8 >= 1 && n % 8 <= 5){
+// 		cout << n % 8 << endl;
+// 	} else if(n % 8 == 6){
+// 		cout << 4 << endl;
+// 	} else if(n % 8 == 7){
+// 		cout << 3 << endl;
+// 	} else if(n % 8 == 0){
+// 		cout << 2 << endl;
+// 	}
+
+//     return 0;
+// }
