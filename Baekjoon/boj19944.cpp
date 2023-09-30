f48856c216112eb034f4cb8d3141becb9b233d84
ChoHJin <cjc9739@naver.com>
2023-09-30 21:47:04 +0900

    0930

---
+++ Baekjoon/boj19944.cpp
@@ -0,0 +1,17 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n, m;
+//     cin >> n >> m;
+
+//     if(m == 1 || m == 2) cout << "NEWBIE!" << endl;
+//     else if(2 < m && m <= n) cout << "OLDBIE!" << endl;
+//     else cout << "TLE!" << endl;
+
+//     return 0;
+// }
