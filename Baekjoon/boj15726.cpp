7588646c571be97f1cd9de5d60bb3f21b35ed8a3
ChoHJin <cjc9739@naver.com>
2023-10-01 23:38:34 +0900

    1001

---
+++ Baekjoon/boj15726.cpp
@@ -0,0 +1,15 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     double a, b, c;
+//     cin >> a >> b >> c;
+
+//     cout << int(max(a / b * c, a * b / c));
+
+//     return 0;
+// }
