773507975391b6f8c0b5b044a554e30a284d6cfc
ChoHJin <cjc9739@naver.com>
2023-10-03 20:59:24 +0900

    1003

---
+++ Baekjoon/boj1735.cpp
@@ -0,0 +1,25 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int gcd(int a, int b) {
+//     if (b == 0) return a;
+//     else return gcd(b, a % b);
+// }
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int a1, a2, b1, b2;
+//     cin >> a1 >> a2 >> b1 >> b2;
+
+//     int x = a1 * b2 + a2 * b1;
+//     int y = a2 * b2;
+
+//     int g = gcd(x, y);
+
+//     cout << x / g << " " << y / g << endl;
+
+//     return 0;
+// }
