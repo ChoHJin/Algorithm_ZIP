79fb9c4c030e767e8b93e36dc7dc376eea1d6af5
ChoHJin <cjc9739@naver.com>
2023-10-06 21:47:30 +0900

    1006

---
+++ Baekjoon/boj14470.cpp
@@ -0,0 +1,17 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int a, b, c, d, e;
+//     cin >> a >> b >> c >> d >> e;
+
+//     if(a < 0) cout << -1 * a * c + b * e + d << endl;
+//     else if(a > 0) cout << (b - a) * e << endl;
+//     else cout << b * e + d << endl;
+
+//     return 0;
+// }
