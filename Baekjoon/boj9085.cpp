2baf9290968bad986b17029a8b118125117af6cc
ChoHJin <cjc9739@naver.com>
2023-10-11 22:32:46 +0900

    1011

---
+++ Baekjoon/boj9085.cpp
@@ -0,0 +1,29 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int t;
+//     cin >> t;
+
+//     while(t--) {
+//         int n;
+//         cin >> n;
+
+//         int sum = 0;
+
+//         for(int i = 0; i < n; i++) {
+//             int num;
+//             cin >> num;
+
+//             sum += num;
+//         }
+
+//         cout << sum << endl;
+//     }
+
+//     return 0;
+// }
