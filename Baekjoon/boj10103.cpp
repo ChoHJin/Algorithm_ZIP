e3f52ce2e6948d52e80fd41075a1b04f2ffbe3f0
ChoHJin <cjc9739@naver.com>
2023-10-14 23:19:45 +0900

    1014

---
+++ Baekjoon/boj10103.cpp
@@ -0,0 +1,29 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n;
+//     cin >> n;
+
+//     int scoreA = 100, scoreB = 100;
+
+//     for(int i = 0; i < n; i++) {
+//         int a, b;
+//         cin >> a >> b;
+
+//         if(a > b) {
+//             scoreB -= a;
+//         } else if(a < b) {
+//             scoreA -= b;
+//         }
+//     }
+
+//     cout << scoreA << endl;
+//     cout << scoreB << endl;
+
+//     return 0;
+// }
