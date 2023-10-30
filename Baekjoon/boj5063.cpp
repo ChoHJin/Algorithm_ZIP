06eed949070a1adb5d73a68fd82421fb89cd1828
ChoHJin <cjc9739@naver.com>
2023-10-30 21:40:35 +0900

    1030

---
+++ Baekjoon/boj5063.cpp
@@ -0,0 +1,22 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int t;
+//     cin >> t;
+
+//     while(t--) {
+//         int r, e, c;
+//         cin >> r >> e >> c;
+
+//         if(r < e - c) cout << "advertise" << endl;
+//         else if(r == e - c) cout << "does not matter" << endl;
+//         else cout << "do not advertise" << endl;
+//     }
+
+//     return 0;
+// }
