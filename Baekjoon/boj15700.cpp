2dd077f245df746d0fe975aaf4cb7cf2a6c7f1ae
ChoHJin <cjc9739@naver.com>
2023-10-31 22:32:47 +0900

    1031

--- Baekjoon/boj15700.cpp
+++ Baekjoon/boj15700.cpp
@@ -1,1 +1,16 @@
-c
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     long long n, m;
+
+//     cin >> n >> m;
+
+//     cout << n * m / 2 << endl;
+
+//     return 0;
+// }
