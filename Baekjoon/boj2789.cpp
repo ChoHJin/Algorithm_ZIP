02f59d54b614c74c7d30c440a9bebc382b081ecb
ChoHJin <cjc9739@naver.com>
2023-11-05 20:52:14 +0900

    1105

---
+++ Baekjoon/boj2789.cpp
@@ -0,0 +1,27 @@
+// #include <iostream>
+// #include <string>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     string s;
+//     cin >> s;
+
+//     string cam = "CAMBRIDGE";
+
+//     for(char c : cam) {
+//         int idx = s.find(c);
+
+//         while(idx != string::npos) {
+//             s.erase(idx, 1);
+//             idx = s.find(c);
+//         }
+//     }
+
+//     cout << s << endl;
+
+//     return 0;
+// }
