8335a757c1e11b1c903a97d8e50aab5c254cd86d
ChoHJin <cjc9739@naver.com>
2023-11-16 04:43:10 +0900

    1116

---
+++ Baekjoon/boj14652.cpp
@@ -0,0 +1,15 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n, m, k;
+//     cin >> n >> m >> k;
+
+//     cout << k / m << " " << k % m << endl;
+
+//     return 0;
+// }
