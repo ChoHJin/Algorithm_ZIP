379122a55be156bb26f613d77e92989591b0e534
ChoHJin <cjc9739@naver.com>
2023-11-19 01:43:05 +0900

    1118

---
+++ Baekjoon/boj3003.cpp
@@ -0,0 +1,22 @@
+// #include <iostream>
+// #include <vector>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     vector<int> v(6);
+//     int c[6] = {1, 1, 2, 2, 2, 8};
+    
+//     for(int i = 0; i < 6; i++) {
+//         cin >> v[i];
+//     }
+
+//     for(int i = 0; i < 6; i++) {
+//         cout << c[i] - v[i] << " ";
+//     }
+
+//     return 0;
+// }
