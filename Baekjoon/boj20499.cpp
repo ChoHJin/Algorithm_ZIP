3be2b0fc895257c3d22856e5e09aa5f9bdc3a5bb
ChoHJin <cjc9739@naver.com>
2023-11-21 23:19:35 +0900

    1121

---
+++ Baekjoon/boj20499.cpp
@@ -0,0 +1,21 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int k, d, a;
+//     char c;
+
+//     cin >> k >> c >> d >> c >> a;
+
+//     if(k + a < d || d == 0) {
+//         cout << "hasu" << endl;
+//     } else {
+//         cout << "gosu" << endl;
+//     }
+
+//     return 0;
+// }
