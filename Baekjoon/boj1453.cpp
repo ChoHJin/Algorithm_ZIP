19d345b6d2080131e2bb08b3bd08ac5ba683e91c
ChoHJin <cjc9739@naver.com>
2023-11-23 23:00:12 +0900

    1123

---
+++ Baekjoon/boj1453.cpp
@@ -0,0 +1,26 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n;
+//     cin >> n;
+
+//     int seat[101] = {0, };
+//     int ans = 0;
+
+//     for(int i = 0; i < n; i++) {
+//         int num;
+//         cin >> num;
+
+//         if(seat[num] == 1) ans++;
+//         else seat[num] = 1;
+//     }
+
+//     cout << ans << endl;
+
+//     return 0;
+// }
