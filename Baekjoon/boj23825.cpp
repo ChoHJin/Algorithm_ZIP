13a54713fe3eca5bfec11e95af44f8ef81402788
ChoHJin <cjc9739@naver.com>
2023-11-24 21:19:45 +0900

    1124

---
+++ Baekjoon/boj23825.cpp
@@ -0,0 +1,16 @@
+// #include <iostream>
+// #include <algorithm>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n, m;
+//     cin >> n >> m;
+
+//     cout << min(n / 2, m / 2) << endl;
+
+//     return 0;
+// }
