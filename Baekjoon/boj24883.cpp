259becac40a60b76c1099eff10f7b0a54032a3f6
ChoHJin <cjc9739@naver.com>
2023-11-30 22:52:27 +0900

    1130

---
+++ Baekjoon/boj24883.cpp
@@ -0,0 +1,16 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     char c;
+//     cin >> c;
+
+//     if(c == 'N' || c == 'n') cout << "Naver D2" << endl;
+//     else cout << "Naver Whale" << endl;
+
+//     return 0;
+// }
