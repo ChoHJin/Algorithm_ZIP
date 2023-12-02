d7282d5ad6df5632b87be3c0081b86385116528d
ChoHJin <cjc9739@naver.com>
2023-12-02 20:27:34 +0900

    1202

---
+++ Baekjoon/boj5338.cpp
@@ -0,0 +1,16 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     cout << "       _.-;;-._" << endl;
+// 	cout << "'-..-'|   ||   |" << endl;	
+// 	cout << "'-..-'|_.-;;-._|" << endl;
+// 	cout << "'-..-'|   ||   |" << endl;
+// 	cout << "'-..-'|_.-''-._|" << endl;
+
+//     return 0;
+// }
