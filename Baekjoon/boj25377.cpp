5b949c5b75286bdd81a62ec3af1bab45df9d8a23
ChoHJin <cjc9739@naver.com>
2023-12-03 22:52:24 +0900

    1203

---
+++ Baekjoon/boj25377.cpp
@@ -0,0 +1,27 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int n;
+//     cin >> n;
+
+//     int ans = 1e9;
+
+//     while(n--) {
+//         int a, b;
+//         cin >> a >> b;
+
+//         if(a > b) continue;
+
+//         ans = min(ans, b);
+//     }
+
+//     if(ans != 1e9) cout << ans << endl;
+//     else cout << -1 << endl;
+
+//     return 0;
+// }
