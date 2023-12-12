7bfcacaf5ac46b3ea14c05717f2565463da9fd91
ChoHJin <cjc9739@naver.com>
2023-12-12 23:28:33 +0900

    1212

---
+++ Baekjoon/boj2948.cpp
@@ -0,0 +1,23 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int mon[12]={31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+// char dow[7][20]={"Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "Monday", "Tuesday"};
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+
+//     int d, m;
+//     cin >> d >> m;
+
+//     for(int i = 0; i < m - 1; i++) {
+//         d += mon[i];
+//     }
+
+//     cout << dow[d % 7] << endl;
+
+
+//     return 0;
+// }
